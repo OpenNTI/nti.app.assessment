c05c7ff8ac20e4d09b40078c3be6b52d0f330f09
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-01-13 08:27:07 +0000

    start adding code to index the content of an assigment feedback. Leave it commented
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@30304 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/assessment/content_search.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Search content utilities.
+
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import interface
+
+from nti.dataserver import interfaces as nti_interfaces
+
+from nti.externalization.oids import to_external_ntiid_oid
+
+from nti.mimetype.mimetype import MIME_BASE
+
+from nti.contentsearch import content_utils
+from nti.contentsearch.search_hits import SearchHit
+from nti.contentsearch import interfaces as search_interfaces
+from nti.contentsearch.search_metadata import SearchTypeMetaData
+from nti.contentsearch.constants import CONTAINER_ID, TYPE, TARGET_MIME_TYPE
+
+from .interfaces import IUsersCourseAssignmentHistoryItemFeedback
+
+assignmentfeedback_ = u'assignmentfeedback'
+ASSIGNMENT_FEEDBACK = u'AssignmentFeedback'
+ASSIGNMENT_FEEDBACK_MIMETYPE = unicode(MIME_BASE + "." + assignmentfeedback_)
+
+class IAssignmentFeedbackResolver(search_interfaces.ITypeResolver,
+								  search_interfaces.IContentResolver,
+								  search_interfaces.INTIIDResolver,
+								  search_interfaces.ICreatorResolver,
+								  search_interfaces.ILastModifiedResolver,
+								  search_interfaces.ICreatedTimeResolver):
+	pass
+
+class IAssignmentFeedbackSearchHit(search_interfaces.ISearchHit):
+	pass
+
+@interface.implementer(IAssignmentFeedbackResolver)
+class _AssignmentFeedbackResolver(object):
+
+	__slots__ = ('obj',)
+
+	def __init__(self, obj):
+		self.obj = obj
+
+	@property
+	def type(self):
+		return assignmentfeedback_
+	
+	@property
+	def content(self):
+		return content_utils.resolve_content_parts(self.obj.body)
+
+	@property
+	def ntiid(self):
+		result = to_external_ntiid_oid(self.obj)
+		return result
+
+	@property
+	def creator(self):
+		result = self.obj.creator
+		if nti_interfaces.IEntity.providedBy(result):
+			result = unicode(result.username)
+		return unicode(result) if result else None
+
+	def lastModified(self):
+		return self.obj.lastModified
+	
+	@property
+	def createdTime(self):
+		return self.obj.createdTime
+
+@interface.implementer(IAssignmentFeedbackSearchHit)
+class _AssignmentFeedbackSearchHit(SearchHit):
+
+	adapter_interface = IAssignmentFeedbackResolver
+
+	def set_hit_info(self, original, score):
+		adapted = super(_AssignmentFeedbackSearchHit, self).set_hit_info(original, score)
+		self[TYPE] = ASSIGNMENT_FEEDBACK
+		self[TARGET_MIME_TYPE] = ASSIGNMENT_FEEDBACK_MIMETYPE
+		self.pop(CONTAINER_ID, None)  # remove container id
+		return adapted
+
+@interface.implementer(search_interfaces.ISearchTypeMetaData)
+def _assignmentfeedback_metadata():
+	# IUsersCourseAssignmentHistoryItemFeedback does not have a mime type
+	# then let's assign one for it
+	return SearchTypeMetaData(Name=assignmentfeedback_,
+							  MimeType=ASSIGNMENT_FEEDBACK_MIMETYPE,
+							  IsUGD=True, Order=99,
+							  Interface=IUsersCourseAssignmentHistoryItemFeedback)
+

b198779045f1a0ce6569fb61e22aa81100749179
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-03-31 22:13:36 +0000

    removed ununsed
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@36140 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/feedback.py
+++ src/nti/app/assessment/feedback.py
@@ -3,14 +3,12 @@
 """
 Implementations of the feedback content types.
 
-$Id$
+.. $Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
-
-import time
 
 from zope import interface
 

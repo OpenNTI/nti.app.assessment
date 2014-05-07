23c983b02b9fa6894ff77155360dc55fbfa87752
Jason Madden <jason.madden@nextthought.com>
2014-05-07 23:34:24 +0000

    per NG, QuestionSet instead of UserData in the ipad hack.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@38644 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/tests/test_adapters.py
+++ src/nti/app/assessment/tests/test_adapters.py
@@ -419,7 +419,7 @@ class TestAssignmentGrading(RegisterAssignmentLayerMixin,ApplicationLayerTest):
 			assert_that( res.json_body, has_entry(self.lesson_page_id,
 												  contains( has_entries( 'Class', 'Assignment',
 																		 'NTIID', self.assignment.__name__,
-																		 'parts',[{'Class': 'QuestionSet'}],
+																		 'parts',[{'Class': 'Assignmentpart'}],
 																		 'category_name', 'no_submit'))))
 
 			res = self.testapp.get(enrollment_assignments)

28b91794aefd686d91062c9fc65d0dbfa6720006
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-08-19 21:10:33 +0000

    add migration skeleton
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@47467 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/assessment/generations/install.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Generations for managing assesments.
+
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 1
+
+from zope.generations.generations import SchemaManager
+
+class _AssessmentSchemaManager(SchemaManager):
+	"""
+	A schema manager that we can register as a utility in ZCML.
+	"""
+	def __init__( self ):
+		super( _AssessmentSchemaManager, self ).__init__(generation=generation,
+														 minimum_generation=generation,
+													  	 package_name='nti.app.assessment.generations')
+
+def evolve( context ):
+	return context

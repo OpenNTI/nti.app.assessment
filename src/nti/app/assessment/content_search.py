8213349198970026a8ee34bbf8a83a5aff5d0910
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-09-07 01:41:40 +0000

    make sure the assigment feedback object have the proper ACL when a course is synced. Use for indexing
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@48814 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/content_search.py
+++ src/nti/app/assessment/content_search.py
@@ -11,6 +11,7 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope import interface
 from zope import lifecycleevent
+from zope.securitypolicy.interfaces import IPrincipalRoleMap
 
 from pyramid.traversal import find_interface
 
@@ -29,8 +30,11 @@ from nti.contentsearch.interfaces import ContentMixinResolver
 from nti.contentsearch.content_utils import resolve_content_parts
 from nti.contentsearch.search_metadata import SearchTypeMetaData
 
+from nti.contenttypes.courses.interfaces import RID_TA
+from nti.contenttypes.courses.interfaces import RID_INSTRUCTOR
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseEnrollments
+from nti.contenttypes.courses.interfaces import ICourseInstanceAvailableEvent
 
 from .interfaces import IUsersCourseAssignmentHistory
 from .interfaces import IUsersCourseAssignmentHistoryItemFeedback
@@ -93,13 +97,17 @@ class _AssignmentFeedbackResolver(object):
 	def acl(self):
 		result = set()
 		creator = self.creator
-		if creator:
+		if creator: # check just in case
 			result.add(self.creator.lower())
 		course = find_interface(self.obj, ICourseInstance)
-		if course is not None:
-			# XXX The instructors are static if we allow changing instructors
-			# this ACL will be come invalid
-			result.update([instructor.id.lower() for instructor in course.instructors])
+		role_map = IPrincipalRoleMap(course, None) 
+		if role_map is not None:
+			principals = role_map.getPrincipalsForRole(RID_INSTRUCTOR) or ()
+			result.update(instructor.id.lower() for instructor in principals)
+			
+			principals = role_map.getPrincipalsForRole(RID_TA) or ()
+			result.update(instructor.id.lower() for instructor in principals)
+		# return
 		return list(result) if result else None
 
 @interface.implementer(IAssignmentFeedbackSearchHit)
@@ -119,16 +127,24 @@ def _assignmentfeedback_metadata():
 	## then let's assign one for it
 	return SearchTypeMetaData(Name=assignmentfeedback_,
 							  MimeType=ASSIGNMENT_FEEDBACK_MIMETYPE,
-							  IsUGD=True, Order=99,
+							  IsUGD=True,
+							  Order=99,
 							  Interface=IUsersCourseAssignmentHistoryItemFeedback)
 
-def on_course_instructors_changed(course):
+
+@component.adapter(ICourseInstanceAvailableEvent)
+def on_course_instance_available(event):
+	course = event.object
 	enrollments = ICourseEnrollments(course)
 	for principal in enrollments.iter_enrollments():
-		assignment_history = component.getMultiAdapter((course, principal),
-														IUsersCourseAssignmentHistory)
+		assignment_history = component.queryMultiAdapter((course, principal),
+														 IUsersCourseAssignmentHistory)
+		if not assignment_history:
+			continue
+		
 		for item in assignment_history.values():
 			if not item.has_feedback():
 				continue
 			for feedback in item.Feedback.values():
+				## force a reindex of the feedback object
 				lifecycleevent.modified(feedback)

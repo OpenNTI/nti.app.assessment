e846ef233de9e9db1f2e7d88c3aee3bce21ee039
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-09-14 17:57:47 +0000

    GET {Assignment}/Savepoint to get a save point
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@49326 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/views/savepoint_views.py
+++ src/nti/app/assessment/views/savepoint_views.py
@@ -9,6 +9,7 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from zope import component
+from zope.schema.interfaces import RequiredMissing
 
 from pyramid.view import view_config
 from pyramid import httpexceptions as hexc
@@ -21,9 +22,9 @@ from nti.appserver.ugd_edit_views import UGDDeleteView
 from nti.assessment.interfaces import IQAssignment
 from nti.assessment.interfaces import IQAssignmentSubmission
 
-from nti.contenttypes.courses.interfaces import ICourseInstance
-
 from nti.dataserver import authorization as nauth
+
+from ..adapters import _find_course_for_assignment
 
 from ..interfaces import IUsersCourseAssignmentSavepoint
 from ..interfaces import IUsersCourseAssignmentSavepoints
@@ -47,16 +48,14 @@ class AssignmentSubmissionSavepointPostView(AbstractAuthenticatedView,
 
 	def _do_call(self):
 		creator = self.remoteUser
-		course = component.queryMultiAdapter( (self.context, creator),
-											  ICourseInstance)
-		if course is None:
+		try:
+			course = _find_course_for_assignment(self.context, creator)
+			if course is None:
+				raise hexc.HTTPForbidden("Must be enrolled in a course.")
+		except RequiredMissing:
 			raise hexc.HTTPForbidden("Must be enrolled in a course.")
-
-		submission = self.readCreateUpdateContentObject(creator)
 		
-		assignment = component.queryUtility(IQAssignment, name=submission.assignmentId)
-		if not assignment:
-			hexc.HTTPUnprocessableEntity("Assignment not found")
+		submission = self.readCreateUpdateContentObject(creator)
 			
 		savepoint = component.getMultiAdapter( (course, submission.creator),
 												IUsersCourseAssignmentSavepoint)
@@ -69,11 +68,35 @@ class AssignmentSubmissionSavepointPostView(AbstractAuthenticatedView,
 		return result
 
 @view_config(route_name="objects.generic.traversal",
+			 context=IQAssignment,
+			 renderer='rest',
+			 request_method='GET',
+			 name="Savepoint")
+class AssignmentSubmissionSavepointGetView(AbstractAuthenticatedView):
+	
+	def __call__(self):
+		creator = self.remoteUser
+		try:
+			course = _find_course_for_assignment(self.context, creator)
+			if course is None:
+				raise hexc.HTTPForbidden("Must be enrolled in a course.")
+		except RequiredMissing:
+			raise hexc.HTTPForbidden("Must be enrolled in a course.")
+				
+		savepoint = component.getMultiAdapter( (course, creator),
+												IUsersCourseAssignmentSavepoint)
+		try:
+			result = savepoint[self.context.assignmentId]
+			return result
+		except KeyError:
+			return hexc.HTTPNotFound()
+
+@view_config(route_name="objects.generic.traversal",
 			 renderer='rest',
 			 context=IUsersCourseAssignmentSavepoints,
 			 permission=nauth.ACT_READ,
 			 request_method='GET')
-class AssignmentSavepointGetView(AbstractAuthenticatedView):
+class AssignmentSavepointsGetView(AbstractAuthenticatedView):
 	"""
 	Students can view their assignment save points as ``path/to/course/AssignmentSavepoints``
 	"""

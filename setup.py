59cdd99aaf42d58678a0c5fc80602f35f686d178
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-09-19 23:07:41 +0000

    fix setup console scripts
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@49722 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ entry_points = {
 	],
 	"console_scripts": [
 		"nti_submission_report = nti.app.assessment.scripts.submission_report:main",
-		"nti_savepoint_migrator = nti.app.assessment.scripts.savepoint_migrator:main"
+		"nti_savepoint_migrator = nti.app.assessment.scripts.savepoint_migrator:main",
 		"nti_extract_assessments = nti.app.assessment.scripts.assignment_policy_extractor:main"
 	],
 }

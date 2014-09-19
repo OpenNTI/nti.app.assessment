b6d082df91c55548c06047d69b4f56f9d7509866
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-09-19 22:38:18 +0000

    delete savepoint submission in case there is a reset
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@49717 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/adapters.py
+++ src/nti/app/assessment/adapters.py
@@ -163,6 +163,8 @@ def _begin_assessment_for_assignment_submission(submission):
 	if submission.assignmentId in assignment_savepoint:
 		old = assignment_savepoint[submission.assignmentId].Submission
 		transfer_upload_ownership(submission, old, copy=True)
+		# delete savepoint submission in case there is a reset
+		del assignment_savepoint[submission.assignmentId]
 	
 	# Ok, now for each part that can be auto graded, do so, leaving all the others
 	# as-they-are

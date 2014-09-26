8964fb16319eb5e29c1ecc434a2d4dae558cd432
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-09-26 02:25:37 +0000

    remove evolution in favor of admin view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@50001 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/views/admin_views.py
+++ src/nti/app/assessment/views/admin_views.py
@@ -9,20 +9,28 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from zope import component
+
 from pyramid.view import view_config
 from pyramid.view import view_defaults
 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
+from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtilsMixin
 
 from nti.app.products.courseware.interfaces import ICourseInstanceEnrollment
 
+from nti.contenttypes.courses.interfaces import ICourseCatalog
 from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseEnrollments
 
 from nti.dataserver import authorization as nauth
+from nti.dataserver.interfaces import IDataserverFolder
 
 from nti.externalization.interfaces import LocatedExternalDict
 
 from ..interfaces import ICourseAssessmentItemCatalog
+from ..interfaces import IUsersCourseAssignmentHistory
+from ..interfaces import IUsersCourseAssignmentSavepoint
 
 @view_config(context=ICourseInstance)
 @view_config(context=ICourseInstanceEnrollment)
@@ -44,4 +52,37 @@ class AllTasksOutlineView(AbstractAuthenticatedView):
 		for item in catalog.iter_assessment_items():
 			unit = item.__parent__
 			result.setdefault(unit.ntiid, []).append(item)
+		return result
+
+@view_config(route_name='objects.generic.traversal',
+			 renderer='rest',
+			 permission=nauth.ACT_MODERATE,
+			 request_method='POST',
+			 context=IDataserverFolder,
+			 name='RemoveMatchedSavePoints')
+class RemovedMatchedSavePointsView(	AbstractAuthenticatedView,
+							   		ModeledContentUploadRequestUtilsMixin):
+	
+	"""
+	Remove savepoint for already submitted assignment(s)
+	"""
+	def _do_call(self):
+		result = LocatedExternalDict()
+		catalog = component.getUtility(ICourseCatalog)
+		for entry in catalog.iterCatalogEntries():
+			course = ICourseInstance(entry)
+			enrollments = ICourseEnrollments(course)
+			for record in enrollments.iter_enrollments():
+				principal = record.Principal
+				history = component.queryMultiAdapter((course, principal), 
+													  IUsersCourseAssignmentHistory)
+				savepoint = component.queryMultiAdapter((course, principal), 
+													    IUsersCourseAssignmentSavepoint)
+				if not savepoint or not history:
+					continue
+				for assignmentId in history.keys():
+					if assignmentId in savepoint:
+						del savepoint[assignmentId]
+						items = result.setdefault(principal.username, [])
+						items.append(assignmentId)
 		return result

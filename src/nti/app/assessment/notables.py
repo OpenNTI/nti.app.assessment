dbf863fb7a8913149fef89be9ead69780a421496
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-11-03 16:46:57 +0000

    target notable
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@52715 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/notables.py
+++ src/nti/app/assessment/notables.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Functions and architecture for general activity streams.
-
-$Id$
+.. $Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
@@ -12,9 +10,9 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import interface
 
-from nti.app.assessment.interfaces import IUsersCourseAssignmentHistoryItemFeedback
-
 from nti.dataserver.interfaces import INotableFilter
+
+from .interfaces import IUsersCourseAssignmentHistoryItemFeedback
 
 @interface.implementer( INotableFilter )
 class AssignmentFeedbackNotableFilter(object):
@@ -23,6 +21,7 @@ class AssignmentFeedbackNotableFilter(object):
 	Feedback is notable if it is on our user's assignments and the feedback
 	is not created by our user.
 	"""
+
 	def __init__(self, context):
 		self.context = context
 
@@ -31,8 +30,6 @@ class AssignmentFeedbackNotableFilter(object):
 		if IUsersCourseAssignmentHistoryItemFeedback.providedBy( obj ):
 			history_item = obj.__parent__
 			submission = history_item.Submission
-
-			if 		submission.creator == user \
-				and obj.creator != user:
+			if submission.creator == user and obj.creator != user:
 				result = True
 		return result

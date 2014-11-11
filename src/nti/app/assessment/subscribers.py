8f25b0eb96f7000a773259bb2e87cca766a8cb51
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-11-11 22:41:20 +0000

    logging
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@53451 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/subscribers.py
+++ src/nti/app/assessment/subscribers.py
@@ -150,5 +150,6 @@ def delete_user_data(user):
 
 @component.adapter(IUser, IObjectRemovedEvent)
 def _on_user_removed(user, event):
+	logger.info("Removing user %s assignment data", user)
 	func = partial(delete_user_data, user=user)
 	run_job_in_all_host_sites(func)

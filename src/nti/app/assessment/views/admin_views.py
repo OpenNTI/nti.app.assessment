9dab25b6b33d086699249556c11f4427265e2c1e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-11-22 05:11:46 +0000

    make it an admin view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@54435 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/views/admin_views.py
+++ src/nti/app/assessment/views/admin_views.py
@@ -10,6 +10,7 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 import csv
+import six
 import urllib
 from io import BytesIO
 
@@ -24,6 +25,9 @@ from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtils
 
 from nti.app.products.courseware.interfaces import ICourseInstanceEnrollment
 
+from nti.assessment.interfaces import IQuestion
+from nti.assessment.interfaces import IQAssignment
+
 from nti.contenttypes.courses.interfaces import ICourseCatalog
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseEnrollments
@@ -33,7 +37,13 @@ from nti.dataserver.interfaces import IDataserverFolder
 
 from nti.externalization.interfaces import LocatedExternalDict
 
+from nti.ntiids.ntiids import TYPE_OID
+from nti.ntiids.ntiids import is_ntiid_of_type
+from nti.ntiids.ntiids import find_object_with_ntiid
+
 from nti.utils.maps import CaseInsensitiveDict
+
+from .._submission import course_submission_report
 
 from ..interfaces import ICourseAssessmentItemCatalog
 from ..interfaces import IUsersCourseAssignmentHistory
@@ -144,6 +154,66 @@ class UnmatchedSavePointsView(AbstractAuthenticatedView):
 						row_data = [ntiid, principal.username, assignmentId]
 						writer.writerow(row_data)
 			
+		stream.flush()
+		stream.seek(0)
+		response.body_file = stream
+		return response
+
+@view_config(route_name='objects.generic.traversal',
+			 renderer='rest',
+			 permission=nauth.ACT_MODERATE,
+			 request_method='GET',
+			 context=IDataserverFolder,
+			 name='CourseSubmissionReport')
+class CourseSubmissionReportView(AbstractAuthenticatedView):
+
+	def __call__(self):
+		catalog = component.getUtility(ICourseCatalog)
+		params = CaseInsensitiveDict(self.request.params)
+		ntiid = params.get('ntiid') or \
+				params.get('entry') or \
+				params.get('course')
+		if not ntiid:
+			raise hexc.HTTPUnprocessableEntity("Must specify a course/entry NTIID")
+		
+		if not is_ntiid_of_type(ntiid, TYPE_OID):
+			try:
+				ntiid = urllib.unquote(ntiid)
+				context = catalog.getCatalogEntry(ntiid)
+			except KeyError:
+				raise hexc.HTTPUnprocessableEntity("Invalid entry NTIID")
+		else:
+			obj = find_object_with_ntiid(ntiid)
+			context = ICourseInstance(obj, None)
+			if context is None:
+				raise hexc.HTTPUnprocessableEntity("Invalid course NTIID")
+		
+		usernames = params.get('usernames') or \
+					params.get('username')
+		if isinstance(usernames, six.string_types):
+			usernames = usernames.split(',')
+		usernames = {x.lower() for x in usernames or ()}
+		
+		assignment = params.get('assignmentId') or \
+					 params.get('assignment')
+		if assignment and component.queryUtility(IQAssignment, name=assignment) is None:
+			raise hexc.HTTPUnprocessableEntity("Invalid assignment")
+
+		question = params.get('questionId') or \
+				   params.get('question')
+		if question and component.queryUtility(IQuestion, name=question) is None:
+			raise hexc.HTTPUnprocessableEntity("Invalid question")
+		
+		response = self.request.response	
+		response.content_encoding = str('identity' )
+		response.content_type = str('text/csv; charset=UTF-8')
+		response.content_disposition = str( 'attachment; filename="report.csv"' )
+		
+		stream = course_submission_report(context=context, 
+							 	 		  question=question,
+							 	 		  usernames=usernames,
+								 		  assignment=assignment)
+		
 		stream.flush()
 		stream.seek(0)
 		response.body_file = stream

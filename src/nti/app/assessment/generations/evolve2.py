52f8d48ad8865ce7e05d0a197a481ad09b4cf8c7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-28 15:47:25 +0000

    add helper function
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@58435 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/generations/evolve2.py
+++ src/nti/app/assessment/generations/evolve2.py
@@ -12,6 +12,24 @@ logger = __import__('logging').getLogger(__name__)
 
 generation = 2
 
+from zope import component
+from zope.component.hooks import site as current_site
+
+from nti.contenttypes.courses.interfaces import ICourseCatalog
+from nti.contenttypes.courses.interfaces import ICourseInstance
+		
+def iter_courses(dataserver_folder):
+	seen = set()
+	sites = dataserver_folder['++etc++hostsites']
+	for site in sites.values():
+		with current_site(site):
+			catalog = component.getUtility(ICourseCatalog)
+			for entry in catalog.iterCatalogEntries():
+				course = ICourseInstance(entry, None)
+				if course is not None and entry.ntiid not in seen:
+					seen.add(entry.ntiid)
+					yield entry, course
+
 def evolve(context):
 	"""
 	Evolve to generation 2

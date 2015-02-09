c667f0589a1361ec3c2fdbc07f0e850f3af0b41a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-09 20:58:15 +0000

    collect feedback from professors
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@59143 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/predicates.py
+++ src/nti/app/assessment/predicates.py
@@ -9,13 +9,18 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from functools import partial
+
 from zope import component
  
 from ZODB.POSException import POSError
  
 from nti.dataserver.interfaces import IUser
  
+from nti.app.products.courseware.interfaces import IPrincipalAdministrativeRoleCatalog
+
 from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseEnrollments
 from nti.contenttypes.courses.interfaces import IPrincipalEnrollments
 
 from nti.metadata.predicates import BasePrincipalObjects
@@ -27,24 +32,62 @@ from .interfaces import IUsersCourseAssignmentHistory
 @component.adapter(IUser)
 class _AssignmentHistoryPrincipalObjects(BasePrincipalObjects):
  
-    def iter_objects(self):
-        result = []
+    def _feedbackitem_collector(self, feedback, creator):
+        for x in feedback.Items:
+            if x.creator == creator:
+                yield x
+
+    def _enrollment_collector(self, result):
         user = self.user
-        def _collector():
-            for enrollments in component.subscribers( (user,), IPrincipalEnrollments):
-                for enrollment in enrollments.iter_enrollments():
-                    try:
-                        course = ICourseInstance(enrollment, None)
-                        items = component.queryMultiAdapter( (course, user),
-                                                              IUsersCourseAssignmentHistory )
+        for enrollments in component.subscribers( (user,), IPrincipalEnrollments):
+            for enrollment in enrollments.iter_enrollments():
+                try:
+                    course = ICourseInstance(enrollment, None)
+                    items = component.queryMultiAdapter( (course, user),
+                                                          IUsersCourseAssignmentHistory )
+                    if not items:
+                        continue
+                    result.append(items)
+                    for item in items:
+                        result.append(item)
+                        result.append(item.Submission)
+                        result.append(item.pendingAssessment)
+                        # check feedback
+                        feedback = item.Feedback
+                        if feedback is not None:
+                            result.append(feedback)
+                            result.extend(self._feedbackitem_collector(feedback, user))
+                except (TypeError, POSError):
+                    continue
+        return result
+    
+    def _feedback_collector(self, result):
+        user = self.user
+        for roles in component.subscribers( (user,), IPrincipalAdministrativeRoleCatalog):
+            for role in roles.iter_administrations():
+                try:
+                    course = ICourseInstance(role, None)
+                    if course is None:
+                        continue
+                    enrollments  = ICourseEnrollments(course)
+                    for record in enrollments.iter_enrollments():
+                        student = IUser(record.principal, None)
+                        if student is None:
+                            continue
+                        items = component.queryMultiAdapter( (course, student),
+                                                             IUsersCourseAssignmentHistory )
                         if not items:
                             continue
-                        result.append(items)
                         for item in items:
-                            result.append(item)
-                            submission = item.Submission
-                            result.append(submission)
-                    except (TypeError, POSError):
-                        continue
-        run_job_in_all_host_sites(_collector)
+                            feedback = item.Feedback
+                            if feedback is not None:
+                                result.extend(self._feedbackitem_collector(feedback, user))
+                except (TypeError, POSError):
+                    continue
+        return result
+    
+    def iter_objects(self):
+        result = []
+        run_job_in_all_host_sites(partial(self._enrollment_collector, result))
+        run_job_in_all_host_sites(partial(self._feedback_collector, result))
         return result

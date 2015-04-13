532288c96760d5ab5998b27af0851e10e0201761
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-04-13 19:02:01 +0000

    Load library during migration
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@63027 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/generations/evolve3.py
+++ src/nti/app/assessment/generations/evolve3.py
@@ -15,39 +15,74 @@ generation = 3
 
 import zope.intid
 
+from zope import interface
+from zope import component
+from zope.component.hooks import site, setHooks
+
 from zope.catalog.interfaces import ICatalog
 
+from nti.contentlibrary.interfaces import IContentPackageLibrary
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
 from nti.dataserver.metadata_index import CATALOG_NAME as METADATA_CATALOG_NAME
 
 from nti.zope_catalog.catalog import ResultSet
 
 from ..index import install_assesment_catalog
 
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+	root = None
+
+	def get_by_oid( self, oid, ignore_creator=False ):
+		resolver = component.queryUtility(IOIDResolver )
+		if resolver is None:
+			logger.warn("Using dataserver without a proper ISiteManager configuration.")
+		else:
+			return resolver.get_object_by_oid( oid, ignore_creator=ignore_creator )
+		return None
+
 def do_evolve(context, generation=generation):
 	logger.info("Assesment evolution %s started", generation);
 	
+	setHooks()
 	conn = context.connection
-	dataserver_folder = conn.root()['nti.dataserver']
-	lsm = dataserver_folder.getSiteManager()
+	ds_folder = conn.root()['nti.dataserver']
+	lsm = ds_folder.getSiteManager()
 	intids = lsm.getUtility(zope.intid.IIntIds)
 	
-	total = 0
-	metadata_catalog = lsm.getUtility(ICatalog, METADATA_CATALOG_NAME)
-	assesment_catalog = install_assesment_catalog(dataserver_folder, intids)
+	mock_ds = MockDataserver()
+	mock_ds.root = ds_folder
+	component.provideUtility( mock_ds, IDataserver )
 	
-	## index all history items
-	ITEM_MT = 'application/vnd.nextthought.assessment.userscourseassignmenthistoryitem'
-	item_intids = metadata_catalog['mimeType'].apply({'any_of': (ITEM_MT,)})
-	results = ResultSet(item_intids, intids, True)
-	for uid, obj in results.iter_pairs():
-		try:
-			assesment_catalog.force_index_doc(uid, obj)
-			total += 1
-		except Exception:
-			logger.warn("Cannot index object with id %s", uid)
-	
-	logger.info('Assesment evolution %s done; %s items(s) indexed',
-				generation, total)
+	with site(ds_folder):
+		assert 	component.getSiteManager() == ds_folder.getSiteManager(), \
+				"Hooks not installed?"
+				
+		total = 0
+		metadata_catalog = lsm.getUtility(ICatalog, METADATA_CATALOG_NAME)
+		assesment_catalog = install_assesment_catalog(ds_folder, intids)
+		
+		## load libray
+		library = component.queryUtility(IContentPackageLibrary)
+		if library is not None:
+			library.syncContentPackages()
+			
+		## index all history items
+		ITEM_MT = 'application/vnd.nextthought.assessment.userscourseassignmenthistoryitem'
+		item_intids = metadata_catalog['mimeType'].apply({'any_of': (ITEM_MT,)})
+		results = ResultSet(item_intids, intids, True)
+		for uid, obj in results.iter_pairs():
+			try:
+				assesment_catalog.force_index_doc(uid, obj)
+				total += 1
+			except Exception:
+				logger.debug("Cannot index object with id %s", uid)
+		
+		logger.info('Assesment evolution %s done; %s items(s) indexed',
+					generation, total)
 
 def evolve(context):
 	"""

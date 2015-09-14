378850cd6965458e820e73971e751f86a5f6c94f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-09-14 20:21:59 +0000

    better strings
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@72948 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/views/assessment_views.py
+++ src/nti/app/assessment/views/assessment_views.py
@@ -440,14 +440,11 @@ from nti.contenttypes.courses.interfaces import ICourseAssessmentItemCatalog
 
 class AssignmentsByOutlineNodeMixin(AbstractAuthenticatedView):
 	
-	_LEGACY_UAS = \
-	(
+	_LEGACY_UAS = (
 		"NTIFoundation DataLoader NextThought/1.0",
-		"NTIFoundation DataLoader NextThought/1.1.0",
-		"NTIFoundation DataLoader NextThought/1.1.1",
+		"NTIFoundation DataLoader NextThought/1.1.",
 		"NTIFoundation DataLoader NextThought/1.2.",
-		"NTIFoundation DataLoader NextThought/1.3.0",
-		"NTIFoundation DataLoader NextThought/1.3.1",
+		"NTIFoundation DataLoader NextThought/1.3.",
 		"NTIFoundation DataLoader NextThought/1.4.0"
 	)
 

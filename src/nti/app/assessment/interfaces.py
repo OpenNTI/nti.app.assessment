b4843ad2bcb2af34052b847199ef996102ec0aed
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-10-14 16:22:56 +0000

    removed unused
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@74707 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/interfaces.py
+++ src/nti/app/assessment/interfaces.py
@@ -9,7 +9,6 @@ Application (integration) level interfaces for assessments.
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-from zope import component
 from zope import interface
 
 from zope.container.constraints import contains

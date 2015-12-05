5627e5aa7391481f137dd57487c5c948ba1d7779
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-12-05 03:07:56 +0000

    WIP: update policies when an item has been updated
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@78377 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/views/view_mixins.py
+++ src/nti/app/assessment/views/view_mixins.py
@@ -9,7 +9,42 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import copy
+
+from zope import component
+
 from nti.appserver.ugd_edit_views import UGDPutView
+
+from nti.assessment.interfaces import IQAssessmentDateContext
+
+from nti.contentlibrary.interfaces import IContentPackage
+
+from nti.contenttypes.courses.interfaces import ICourseCatalog
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import SUPPORTED_DATE_KEYS
+
+from nti.contenttypes.courses.utils import get_course_packages
+
+from nti.traversal.traversal import find_interface
+
+from .._utils import get_course_from_request
+
+def get_courses_from_assesment(assesment):
+	package = find_interface(assesment, IContentPackage, strict=False)
+	if package is None:
+		return ()
+
+	# Nothing. OK, maybe we're an instructor?
+	catalog = component.queryUtility(ICourseCatalog)
+	if catalog is None:
+		return ()
+
+	result = []
+	for entry in catalog.iterCatalogEntries():
+		packages = get_course_packages(entry)
+		if package in packages:
+			result.append(ICourseInstance(entry))
+	return result
 
 class AssessmentPutView(UGDPutView):
 
@@ -18,4 +53,40 @@ class AssessmentPutView(UGDPutView):
 		result = UGDPutView.readInput(self, value=value)
 		result.pop('ntiid', None)
 		result.pop('NTIID', None)
+		return result
+
+	def updateContentObject(self, contentObject, externalValue, set_id=False,
+							notify=True, pre_hook=None):
+		context = get_course_from_request(self.request)
+		if context is not None:
+			# remove policy keys to avoid updating
+			# fields in the actual assessment object
+			backupData = copy.copy(externalValue)
+			for key in SUPPORTED_DATE_KEYS:
+				externalValue.pop(key, None)
+		else:
+			backupData = externalValue
+
+		# update
+		result = UGDPutView.updateContentObject(self,
+												notify=notify,
+												set_id=set_id,
+												pre_hook=pre_hook,
+												externalValue=backupData,
+												contentObject=contentObject)
+
+		# find all courses if context is not provided
+		if context is None:
+			courses = get_courses_from_assesment(contentObject)
+		else:
+			courses = (context,)
+
+		# update course policies
+		ntiid = contentObject.ntiid
+		for key in SUPPORTED_DATE_KEYS:
+			if key not in backupData:
+				continue
+			for course in courses:
+				dates = IQAssessmentDateContext(course)
+				dates.set(ntiid, key, backupData[key])
 		return result

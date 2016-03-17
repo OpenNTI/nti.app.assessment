c232842f709474237c32ad957be5a59a5d13a03e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-03-17 01:24:06 +0000

    overwrite jz changes for a bit
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@84810 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/scripts/nti_fix_assessment_leaks.py
+++ src/nti/app/assessment/scripts/nti_fix_assessment_leaks.py
@@ -11,7 +11,6 @@ logger = __import__('logging').getLogger(__name__)
 
 import os
 import sys
-import itertools
 import argparse
 from collections import defaultdict
 
@@ -44,69 +43,43 @@ from nti.dataserver.utils.base_script import create_context
 from nti.intid.common import removeIntId
 
 from nti.metadata import dataserver_metadata_catalog
-
+				
 from nti.site.hostpolicy import get_all_host_sites
 
-from nti.site.site import getSite
 from nti.site.utils import registerUtility
 from nti.site.utils import unregisterUtility
 
 from nti.traversal.traversal import find_interface
 
-def _get_content_packages():
-	library = component.getUtility(IContentPackageLibrary)
-	for package in library.contentPackages:
-		yield package
-
-def _master_data_collector( intids ):
+def _master_data_collector():
 	seen = set()
 	registered = {}
-	# The site of the assessment's content unit.
-	home_site = {}
-	home_intid = {}
-	home_obj = {}
 	containers = defaultdict(list)
-
-	def recur(unit, unit_site):
+		
+	def recur(unit):
 		for child in unit.children or ():
-			recur(child, unit_site)
+			recur(child)
 		container = IQAssessmentItemContainer(unit)
 		for item in container.assessments():
 			containers[item.ntiid].append(container)
-			home_site[item.ntiid] = unit_site
-			home_obj[item.ntiid] = item
-			home_intid[item.ntiid] = intids.queryId( item )
-
-	# First, get all the items found through package library.
-	for site in itertools.chain( (getSite(),), get_all_host_sites() ):
-		with current_site(site):
-			for package in _get_content_packages():
-				if package.ntiid not in seen:
-					seen.add(package.ntiid)
-					recur(package, site)
-
-	for site in itertools.chain( (getSite(),), get_all_host_sites() ):
+	
+	for site in get_all_host_sites():
 		registry = site.getSiteManager()
 		for ntiid, item in list(registry.getUtilitiesFor(IQAssessment)):
 			if ntiid not in registered:
 				registered[ntiid] = (site, item)
-			obj_home_site = home_site.get( item.ntiid )
-			# Can we just check 'is'?
-			if 		obj_home_site is not None \
-				and item != home_obj.get( item.ntiid ):
-				logger.info( 'Unregistering asset (home=%s) (site=%s) (ntiid=%s)',
-							obj_home_site.__name__,
-							site.__name__,
-							item.ntiid )
-				registry = site.getSiteManager()
-				provided = iface_of_assessment(item)
-				unregisterUtility(registry, provided=provided, name=ntiid)
-
+				
 		for ntiid, item in list(registry.getUtilitiesFor(IQInquiry)):
 			if ntiid not in registered:
 				registered[ntiid] = (site, item)
+				
+		with current_site(site):
+			for package in yield_sync_content_packages():
+				if package.ntiid not in seen:
+					seen.add(package.ntiid)
+					recur(package)
 
-	return registered, containers, home_site
+	return registered, containers
 
 def _get_data_item_counts(intids):
 	count = defaultdict(list)
@@ -123,22 +96,16 @@ def _get_data_item_counts(intids):
 def _process_args(verbose=True, with_library=True):
 	library = component.getUtility(IContentPackageLibrary)
 	library.syncContentPackages()
-
+	
 	intids = component.getUtility(IIntIds)
 	count = _get_data_item_counts(intids)
 	logger.info('%s item(s) counted', len(count))
-
-	registered_objs, all_containers, home_site = _master_data_collector( intids )
+	
+	all_registered, all_containers = _master_data_collector()
 
 	result = 0
 	catalog = get_library_catalog()
 	for ntiid, data in count.items():
-		site = home_site.get( ntiid )
-		if site is None:
-			# ??, unregister these?
-			logger.info( 'No home site found for %s', ntiid )
-		# TODO: If our home site is a root site (dataserver2), and we do
-		# not have an intid, we should unindex the indexed object (..data).
 		if len(data) <= 1:
 			continue
 		logger.warn("%s has %s duplicate(s)", ntiid, len(data) - 1)
@@ -146,33 +113,27 @@ def _process_args(verbose=True, with_library=True):
 		# find registry and registered objects
 		context = data[0]  # pivot
 		provided = iface_of_assessment(context)
-		things = registered_objs.get(ntiid)
+		things = all_registered.get(ntiid)
 		if not things:
-			logger.warn("No registration found for %s", ntiid)
+			logger.warn("No registration found for %s", ntiid)	
 			for item in data:
 				iid = intids.queryId(item)
 				if iid is not None:
 					removeIntId(item)
 			continue
 
-		# Be careful to not register in a sub-site (non content unit site).
-		registered_site, registered = things
-		site = home_site.get( ntiid )
-		if site is None:
-			# ??, unregister these?
-			logger.info( 'No home site found for %s (registered_site=%s)', ntiid, registered_site )
-			site = registered_site
+		site, registered = things
 		registry = site.getSiteManager()
-
+		
 		# if registered has been found.. check validity
 		ruid = intids.queryId(registered)
 		if ruid is None:
-			logger.warn("Invalid registration for %s", ntiid)
+			logger.warn("Invalid registration for %s", ntiid)		
 			unregisterUtility(registry, provided=provided, name=ntiid)
 			# register a valid object
 			registered = context
 			ruid = intids.getId(context)
-			#registerUtility(registry, context, provided, name=ntiid)
+			registerUtility(registry, context, provided, name=ntiid)
 
 		for item in data:
 			doc_id = intids.getId(item)

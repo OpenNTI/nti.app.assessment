b954a90a7bf6f639e969ece4b3364756b146df92
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-03-31 22:38:11 +0000

    WIP: eval exporter
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@85744 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/assessment/exporter.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import re
+from StringIO import StringIO
+from collections import Mapping
+
+import simplejson
+
+from zope import interface
+
+from nti.app.assessment.common import get_unit_assessments
+
+from nti.common.proxy import removeAllProxies
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseSectionExporter
+
+from nti.contenttypes.courses.common import get_course_packages
+
+from nti.contenttypes.courses.utils import get_parent_course
+
+from nti.externalization.externalization import to_external_object
+
+from nti.externalization.interfaces import StandardExternalFields
+
+from nti.mimetype import decorateMimeType
+
+from nti.ntiids.ntiids import TYPE_OID, find_object_with_ntiid
+from nti.ntiids.ntiids import is_ntiid_of_type
+
+OID = StandardExternalFields.OID
+ITEMS = StandardExternalFields.ITEMS
+NTIID = StandardExternalFields.NTIID
+MIMETYPE = StandardExternalFields.MIMETYPE
+CONTAINER_ID = StandardExternalFields.CONTAINER_ID
+
+def safe_filename(s):
+	return re.sub(r'[/<>:"\\|?*]+', '_', s) if s else s
+
+@interface.implementer(ICourseSectionExporter)
+class AssessmentsExporter(object):
+
+	def adjuster(self, result):
+		if isinstance(result, Mapping):
+			for name, value in list(result.items()):
+				if name in (OID, CONTAINER_ID, 'containerId'):
+					result.pop(name, None)
+				elif name == NTIID:
+					if is_ntiid_of_type(value, TYPE_OID):
+						result.pop(name, None)
+					else:
+						if MIMETYPE not in result:
+							obj = find_object_with_ntiid(value)
+							if obj is not None:
+								decorateMimeType(obj, result)
+				else:
+					self.adjuster(value)
+		elif isinstance(result, (list, tuple)):
+			for value in result:
+				self.adjuster(value)
+		return result
+
+	def mapped(self, package, items):
+		def _recur(unit, items):
+			# all units have a map
+			items[unit.ntiid] = dict()
+			# collect evaluations
+			evaluations = dict()
+			for evaluation in get_unit_assessments(unit):
+				evaluation = removeAllProxies(evaluation)
+				ext_obj = self.adjuster(to_external_object(evaluation, decorate=False))
+				evaluations[evaluation.ntiid] = ext_obj
+				if MIMETYPE not in ext_obj:
+					decorateMimeType(evaluation, ext_obj)
+			items[unit.ntiid]['AssessmentItems'] = evaluations
+			# create new items for children
+			child_items = items[unit.ntiid][ITEMS] = dict()
+			for child in unit.children:	
+				_recur(child, child_items)
+			# remove empty
+			if not evaluations and not child_items:
+				items.pop(unit.ntiid, None)
+			else:
+				if not evaluations:
+					items[unit.ntiid].pop('AssessmentItems', None)
+				# add legacy
+				items[unit.ntiid][NTIID] = unit.ntiid
+				items[unit.ntiid]['filename'] = safe_filename(unit.ntiid) + '.html'
+		_recur(package, items)
+		if package.ntiid in items:
+			items[package.ntiid]['filename'] = 'index.html'
+
+	def export(self, context, filer):
+		result = {}
+		course = ICourseInstance(context)
+		course = get_parent_course(course)
+		items = result[ITEMS] = dict()
+		for package in get_course_packages(course):
+			self.mapped(package, items)
+		# export to json
+		source = StringIO()
+		simplejson.dump(result, source, indent=4, sort_keys=True)
+		source.seek(0)
+		# save in filer
+		filer.save("assessment_index.json", source, 
+				   contentType="application/json", overwrite=True)

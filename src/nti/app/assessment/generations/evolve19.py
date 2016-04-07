4ea280306f2ab8dd39f642e39baa3b1a0805c171
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-04-07 00:25:18 +0000

    add evolution 19.. replaces evol 18
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@86101 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/assessment/generations/evolve19.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 19
+
+from zope import component
+from zope import interface
+
+from zope.component.hooks import site
+from zope.component.hooks import setHooks
+from zope.component.hooks import site as current_site
+
+from zope.intid.interfaces import IIntIds
+
+from nti.assessment.interfaces import IQPoll
+from nti.assessment.interfaces import IQuestion
+from nti.assessment.interfaces import IQEditable
+from nti.assessment.interfaces import IQAssignment
+from nti.assessment.interfaces import IQEvaluation
+
+from nti.coremetadata.interfaces import IPublishable
+from nti.coremetadata.interfaces import INoPublishLink 
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
+
+from nti.recorder import get_recorder_catalog
+
+from nti.site.hostpolicy import get_all_host_sites
+
+def _process_items(registry, intids, seen):
+	catalog = get_recorder_catalog()
+	for _, item in list(registry.getUtilitiesFor(IQEvaluation)):
+		if IQEditable.providedBy(item):
+			continue
+		doc_id = intids.queryId(item)
+		if doc_id is not None and doc_id not in seen:
+			seen.add(doc_id)
+			if IPublishable.providedBy(item) and not item.is_published():
+				item.publish()
+				catalog.index_doc(doc_id, item)
+				interface.alsoProvides(item, INoPublishLink)
+			if 		IQAssignment.providedBy(item) \
+				or	IQuestion.providedBy(item) \
+				or	IQPoll.providedBy(item):
+				for x in item.parts or ():
+					x.__parent__ = item  # take ownership
+
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+	root = None
+
+	def get_by_oid(self, oid, ignore_creator=False):
+		resolver = component.queryUtility(IOIDResolver)
+		if resolver is None:
+			logger.warn("Using dataserver without a proper ISiteManager configuration.")
+		else:
+			return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+		return None
+
+def do_evolve(context, generation=generation):
+	logger.info("Assessment evolution %s started", generation);
+
+	setHooks()
+	conn = context.connection
+	ds_folder = conn.root()['nti.dataserver']
+	lsm = ds_folder.getSiteManager()
+	intids = lsm.getUtility(IIntIds)
+	
+	mock_ds = MockDataserver()
+	mock_ds.root = ds_folder
+	component.provideUtility(mock_ds, IDataserver)
+
+	with current_site(ds_folder):
+		assert 	component.getSiteManager() == ds_folder.getSiteManager(), \
+				"Hooks not installed?"
+
+		seen = set()
+		for site in get_all_host_sites():
+			with current_site(site):
+				registry = component.getSiteManager()
+				_process_items(registry, intids, seen)
+
+	component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
+	logger.info('Assessment evolution %s done.', generation)
+
+def evolve(context):
+	"""
+	Evolve to generation 18 by publishing evaluations
+	"""
+	do_evolve(context)

f2f36e598c98505a3a89ed5618b59ce6c2a6ab6f
Josh Zuech <josh.zuech@nextthought.com>
2016-05-27 21:20:59 +0000

    Move randomize decorators to request based; do not randomize for instructors/editors
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@89467 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/assessment/decorators/randomized.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.app.assessment.decorators import _get_course_from_assignment
+
+from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
+
+from nti.appserver.pyramid_authorization import has_permission
+
+from nti.assessment.randomized import randomize
+from nti.assessment.randomized import shuffle_list
+from nti.assessment.randomized import must_randomize
+from nti.assessment.randomized import shuffle_matching_part_solutions
+from nti.assessment.randomized import shuffle_multiple_choice_part_solutions
+from nti.assessment.randomized import shuffle_multiple_choice_multiple_answer_part_solutions
+
+from nti.assessment.randomized.interfaces import IRandomizedQuestionSet
+from nti.assessment.randomized.interfaces import IQRandomizedMatchingPart
+from nti.assessment.randomized.interfaces import IQRandomizedOrderingPart
+from nti.assessment.randomized.interfaces import IQRandomizedMultipleChoicePart
+from nti.assessment.randomized.interfaces import IQRandomizedMultipleChoiceMultipleAnswerPart
+
+from nti.contenttypes.courses.utils import is_course_instructor
+
+from nti.dataserver.authorization import ACT_CONTENT_EDIT
+
+from nti.externalization.interfaces import IExternalObjectDecorator
+
+class _AbstractNonEditorRandomizingDecorator(AbstractAuthenticatedRequestAwareDecorator):
+	"""
+	An abstract decorator that only randomizes if we do not have an instructor, an
+	editor, or a non-randomized interface.
+	"""
+
+	def _predicate(self, context, result):
+		user = self.remoteUser
+		course = _get_course_from_assignment(context, user, request=self.request)
+		return 		self._is_authenticated \
+				and not has_permission(ACT_CONTENT_EDIT, context, self.request) \
+				and not is_course_instructor( course, user ) \
+				and must_randomize( context )
+
+@interface.implementer(IExternalObjectDecorator)
+@component.adapter(IQRandomizedMatchingPart)
+class _QRandomizedMatchingPartDecorator(_AbstractNonEditorRandomizingDecorator):
+
+	def _do_decorate_external(self, context, result):
+		generator = randomize(context=context)
+		if generator is not None:
+			values = list(result['values'])
+			shuffle_list(generator, result['values'])
+			shuffle_matching_part_solutions(randomize(context=context), # new generator
+											 values,
+											 result['solutions'])
+
+@interface.implementer(IExternalObjectDecorator)
+@component.adapter(IQRandomizedOrderingPart)
+class _QRandomizedOrderingPartDecorator(_QRandomizedMatchingPartDecorator):
+	pass
+
+@interface.implementer(IExternalObjectDecorator)
+@component.adapter(IQRandomizedMultipleChoicePart)
+class _QRandomizedMultipleChoicePartDecorator(_AbstractNonEditorRandomizingDecorator):
+
+	def _do_decorate_external(self, context, result):
+		generator = randomize(context=context)
+		if generator is not None:
+			solutions = result['solutions']
+			choices = list(result['choices'])
+			shuffle_list(generator, result['choices'])
+			shuffle_multiple_choice_part_solutions(randomize(context=context), #  new generator
+													choices,
+													solutions)
+
+@interface.implementer(IExternalObjectDecorator)
+@component.adapter(IQRandomizedMultipleChoiceMultipleAnswerPart)
+class _QRandomizedMultipleChoiceMultipleAnswerPartDecorator(_AbstractNonEditorRandomizingDecorator):
+
+	def _do_decorate_external(self, context, result):
+		generator = randomize(context=context)
+		if generator is not None:
+			choices = list(result['choices'])
+			shuffle_list(generator, result['choices'])
+			shuffle_multiple_choice_multiple_answer_part_solutions(randomize(context=context), #  new generator
+																	choices,
+																	result['solutions'])
+
+@interface.implementer(IExternalObjectDecorator)
+@component.adapter(IRandomizedQuestionSet)
+class _QRandomizedQuestionSetDecorator(_AbstractNonEditorRandomizingDecorator):
+
+	def _do_decorate_external(self, context, result):
+		generator = randomize(context=context)
+		questions = result.get('questions', ())
+		if generator and questions:
+			shuffle_list(generator, questions)

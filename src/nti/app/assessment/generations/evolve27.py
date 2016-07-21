5a883e72e0b2bf2fa5d7935a9b941a3f20883e32
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-07-21 00:39:43 +0000

    add evolve 27. set lineage for history items
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@92740 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/assessment/generations/evolve27.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+generation 27.
+
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 27
+
+from zope import component
+from zope import interface
+
+from zope.component.hooks import site
+from zope.component.hooks import setHooks
+
+from zope.catalog.interfaces import ICatalog
+
+from zope.intid.interfaces import IIntIds
+
+from nti.app.assessment.common import set_assessed_lineage
+
+from nti.app.assessment.interfaces import IUsersCourseAssignmentHistoryItem
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
+
+from nti.dataserver.metadata_index import CATALOG_NAME as METADATA_CATALOG_NAME
+
+from nti.zope_catalog.catalog import ResultSet
+
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+	root = None
+
+	def get_by_oid(self, oid, ignore_creator=False):
+		resolver = component.queryUtility(IOIDResolver)
+		if resolver is None:
+			logger.warn("Using dataserver without a proper ISiteManager configuration.")
+		else:
+			return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+		return None
+
+def do_evolve(context, generation=generation):
+	logger.info("Assessment evolution %s started", generation);
+
+	setHooks()
+	conn = context.connection
+	ds_folder = conn.root()['nti.dataserver']
+	lsm = ds_folder.getSiteManager()
+	intids = lsm.getUtility(IIntIds)
+
+	mock_ds = MockDataserver()
+	mock_ds.root = ds_folder
+	component.provideUtility(mock_ds, IDataserver)
+
+	with site(ds_folder):
+		assert 	component.getSiteManager() == ds_folder.getSiteManager(), \
+				"Hooks not installed?"
+
+		total = 0
+		metadata_catalog = lsm.getUtility(ICatalog, METADATA_CATALOG_NAME)
+
+		MIME_TYPES = ('application/vnd.nextthought.assessment.userscourseassignmenthistoryitem',)
+		item_intids = metadata_catalog['mimeType'].apply({'any_of': MIME_TYPES})
+		for item in ResultSet(item_intids or (), intids, True):
+			if IUsersCourseAssignmentHistoryItem.providedBy(item):
+				pending = item.pendingAssessment
+				if pending is not None:
+					set_assessed_lineage(pending)
+
+	component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
+	logger.info('Assessment evolution %s done; %s items(s) indexed', generation, total)
+
+def evolve(context):
+	"""
+	Evolve to generation 27 by setting lineage for all Assignment History Item
+	"""
+	do_evolve(context)

c24f905caf4b5a10c4566cd366e0186ece4cbf3b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-08-24 20:34:35 +0000

    define link maker
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@95315 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/assessment/renderers.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from zope.location.interfaces import ILocation
+
+from nti.app.assessment.utils import get_course_from_request
+
+from nti.appserver.pyramid_renderers_edit_link_decorator import DefaultEditLinkMaker
+
+from nti.appserver.interfaces import IEditLinkMaker
+
+from nti.assessment.interfaces import IQEvaluation
+
+from nti.links.links import Link
+
+@component.adapter(IQEvaluation)
+@interface.implementer(IEditLinkMaker)
+class AssesmentEditLinkMaker(DefaultEditLinkMaker):
+    
+    def make(self, context, request=None, allow_traversable_paths=True, link_method=None):
+        context = self.context if context is None else context
+        course = get_course_from_request(request)
+        if course is not None:
+            link = Link(course,
+                        rel='edit',
+                        elements=('Assessments', context.ntiid),
+                        method=link_method)
+            link.__parent__ = context.__parent__
+            link.__name__ = context.__name__
+            interface.alsoProvides(link, ILocation)
+            return link
+        # default
+        return DefaultEditLinkMaker.make(self, 
+                                         context, 
+                                         request=request, 
+                                         allow_traversable_paths=allow_traversable_paths,
+                                         link_method=link_method)

c40e18f40f9b194e87f25cfea7b82d2ef8b75508
Josh Zuech <josh.zuech@nextthought.com>
2016-09-16 03:33:44 +0000

    Swap evolves 30 and 31
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@96896 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/generations/evolve31.py
+++ src/nti/app/assessment/generations/evolve31.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+generation 30.
+
 .. $Id$
 """
 
@@ -14,35 +16,21 @@ generation = 31
 from zope import component
 from zope import interface
 
-from zope.component.hooks import site
 from zope.component.hooks import setHooks
 from zope.component.hooks import site as current_site
 
-from nti.app.assessment.common import get_courses
-from nti.app.assessment.common import regrade_evaluation
-from nti.app.assessment.common import evaluation_submissions
-from nti.app.assessment.common import get_resource_site_name
-from nti.app.assessment.common import get_evaluation_courses
+from zope.intid.interfaces import IIntIds
 
-from nti.assessment.common import iface_of_assessment
+from nti.app.assessment import get_evaluation_catalog
 
-from nti.assessment.interfaces import IQuestion
-from nti.assessment.interfaces import IQAssignment
+from nti.app.assessment.index import IX_CONTAINERS
 
-from nti.contentlibrary.interfaces import IContentPackageLibrary
-
-from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
-
-from nti.coremetadata.interfaces import IRecordable
+from nti.assessment.interfaces import IQEvaluation
 
 from nti.dataserver.interfaces import IDataserver
 from nti.dataserver.interfaces import IOIDResolver
 
-from nti.site.hostpolicy import get_host_site
 from nti.site.hostpolicy import get_all_host_sites
-
-from nti.site.utils import registerUtility
-from nti.site.utils import unregisterUtility
 
 @interface.implementer(IDataserver)
 class MockDataserver(object):
@@ -57,83 +45,16 @@ class MockDataserver(object):
 			return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
 		return None
 
-def check_registry( registry, evaluation, site_name, entry_ntiid ):
-	"""
-	Check if registered object is same as given object; if not register
-	given object.
-	"""
-	provided = iface_of_assessment(evaluation)
-	registered = registry.queryUtility(provided, name=evaluation.ntiid)
-	needs_regrade = False
-	if registered is not None and registered != evaluation:
-		logger.info( "[%s] Item does not match registry (%s), re-registering (course=%s)",
-					 site_name, evaluation.ntiid, entry_ntiid )
-		if IQuestion.providedBy( evaluation ):
-			for part, registered_part in zip( evaluation.parts or (),
-											  registered.parts or () ):
-				if part.randomized != registered_part.randomized:
-					needs_regrade = True
-		# TODO: Remove intid?
-		unregisterUtility(registry, provided=provided, name=evaluation.ntiid)
-		registerUtility(registry, evaluation, provided=provided,
-						name=evaluation.ntiid, event=False)
-	return needs_regrade
-
-def _is_obj_locked(context):
-	return IRecordable.providedBy(context) and context.isLocked()
-
-def _get_assignment_courses( assignment ):
-	"""
-	Get all courses and subinstances for our assignment.
-	"""
-	courses = get_evaluation_courses( assignment )
-	result = set()
-	for course in courses or ():
-		all_courses = get_courses( course, subinstances=True )
-		result.update( all_courses )
-	return tuple( result )
-
-def _update_registered_objects(site_registry, seen, site_name):
-	"""
-	Loop through all locked assignments making sure underlying
-	question_sets and questions are the registered objects. If
-	not register and regrade if randomized status is different.
-	"""
-	for ntiid, item in list(site_registry.getUtilitiesFor(IQAssignment)):
-		if ntiid in seen:
-			continue
-		seen.add(ntiid)
-		if not _is_obj_locked( item ):
-			continue
-
-		courses = _get_assignment_courses( item )
-		if courses:
-			needs_regrade = False
-			# Arbitrary course for site registry and logging
-			course = courses[0]
-			course_site_name = get_resource_site_name(course)
-			registry = get_host_site(course_site_name).getSiteManager()
-			entry_ntiid = ICourseCatalogEntry( course ).ntiid
-			for part in item.parts or ():
-				question_set = part.question_set
-				check_registry( registry, question_set, course_site_name, entry_ntiid )
-				for question in question_set.questions or ():
-					part_regrade = check_registry( registry, question,
-												   course_site_name, entry_ntiid )
-					if part_regrade:
-						needs_regrade = True
-			if needs_regrade:
-				for course in courses:
-					entry = ICourseCatalogEntry( course, None )
-					entry_ntiid = getattr( entry, 'ntiid', '' )
-					submissions = evaluation_submissions( item, course )
-					submission_count = len( submissions or () )
-					logger.info( "[%s] Regrading assessment (%s) (course=%s) (submission_count=%s)",
-								 course_site_name, ntiid, entry_ntiid, submission_count )
-					regrade_evaluation(item, course)
-		else:
-			logger.warn( '[%s] No courses found for assignment (%s)',
-						 site_name, ntiid )
+def _update_evaluation_index( registry, container_index, intids, seen ):
+	count = 0
+	for ntiid, item in list(registry.getUtilitiesFor(IQEvaluation)):
+		if ntiid not in seen:
+			seen.add( ntiid )
+			doc_id = intids.queryId(item)
+			if doc_id is not None:
+				container_index.index_doc( doc_id, item )
+				count +=1
+	return count
 
 def do_evolve(context, generation=generation):
 	logger.info("Assessment evolution %s started", generation);
@@ -141,6 +62,8 @@ def do_evolve(context, generation=generation):
 	setHooks()
 	conn = context.connection
 	ds_folder = conn.root()['nti.dataserver']
+	lsm = ds_folder.getSiteManager()
+	intids = lsm.getUtility(IIntIds)
 
 	mock_ds = MockDataserver()
 	mock_ds.root = ds_folder
@@ -150,27 +73,21 @@ def do_evolve(context, generation=generation):
 		assert 	component.getSiteManager() == ds_folder.getSiteManager(), \
 				"Hooks not installed?"
 
-		# Load library
-		library = component.queryUtility(IContentPackageLibrary)
-		if library is not None:
-			library.syncContentPackages()
-
+		catalog = get_evaluation_catalog()
+		container_index = catalog[IX_CONTAINERS]
 		seen = set()
 		for site in get_all_host_sites():
 			with current_site(site):
-				site_name = site.__name__
 				registry = component.getSiteManager()
-				_update_registered_objects(registry, seen, site_name)
-
-		seen.clear()
+				count = _update_evaluation_index(registry, container_index, intids, seen)
+				logger.info( '[%s] Indexed %s objects', site.__name__, count )
 
 	component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
 	logger.info('Assessment evolution %s done.', generation)
 
 def evolve(context):
 	"""
-	Evolve to generation 31 by looking for locked assignments and make sure the
-	underlying questions and question sets are the items registered.
+	Evolve to generation 31 by re-indexing evaluation objects in the
+	container index in order to make sure all course/packages are correct.
 	"""
 	do_evolve(context)
-

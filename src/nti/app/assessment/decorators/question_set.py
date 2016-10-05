fd8ee653726b3daadcf01e772d4698f931b67de0
Josh Zuech <josh.zuech@nextthought.com>
2016-10-05 19:49:14 +0000

    Add question-count decorator for question sets
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@98197 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/assessment/decorators/question_set.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.assessment.interfaces import IQuestionSet
+
+from nti.externalization.interfaces import StandardExternalFields
+from nti.externalization.interfaces import IExternalObjectDecorator
+
+from nti.externalization.singleton import SingletonDecorator
+
+OID = StandardExternalFields.OID
+LINKS = StandardExternalFields.LINKS
+
+@component.adapter(IQuestionSet)
+@interface.implementer(IExternalObjectDecorator)
+class _NTIQuestionSetCountDecorator(object):
+
+	__metaclass__ = SingletonDecorator
+
+	def decorateExternalObject(self, original, external):
+		external.pop('question_count', None)
+		question_count = getattr( original, 'draw', None ) or len( original.questions )
+		external[u'question-count'] = str( question_count )

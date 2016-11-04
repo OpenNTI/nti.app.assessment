32183aad2cc0354e12a05e41db0c4dd0a4e879ed
Josh Zuech <josh.zuech@nextthought.com>
2016-11-04 14:52:16 +0000

    Make sure we delete question set before assignment is deleted
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@100087 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/views/evaluation_views.py
+++ src/nti/app/assessment/views/evaluation_views.py
@@ -1177,6 +1177,12 @@ class AssignmentPutView(NewAndLegacyPutView):
 # DELETE views
 
 def delete_evaluation(evaluation):
+	# Clean up question sets under assignments
+	if IQAssignment.providedBy( evaluation ):
+		for part in evaluation.parts or ():
+			if part.question_set is not None:
+				delete_evaluation( part.question_set )
+
 	# delete from evaluations
 	course = find_interface(evaluation, ICourseInstance, strict=False)
 	evaluations = ICourseEvaluations(course, None)
@@ -1191,12 +1197,6 @@ def delete_evaluation(evaluation):
 		site_name = get_resource_site_name(course) or getSite().__name__
 		registry = get_host_site(site_name).getSiteManager()
 		unregisterUtility(registry, provided=provided, name=evaluation.ntiid)
-
-	# Clean up question sets under assignments
-	if IQAssignment.providedBy( evaluation ):
-		for part in evaluation.parts or ():
-			if part.question_set is not None:
-				delete_evaluation( part.question_set )
 
 @view_config(route_name="objects.generic.traversal",
 			 context=IQEvaluation,

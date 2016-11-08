f054ffa91b5c32b735d28ed91be347035bd1a64f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-08 19:35:06 +0000

    when not backing editable evaluations,  make sure we take a hash of the current NTIID and use it as the specific part for a new NTIID to make sure there are fewer collisions when importing back
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@100343 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/evaluations/exporter.py
+++ src/nti/app/assessment/evaluations/exporter.py
@@ -9,6 +9,9 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import hashlib
+from collections import Mapping
+
 from zope import interface
 
 from nti.app.assessment.evaluations.utils import export_evaluation_content
@@ -28,19 +31,51 @@ from nti.contenttypes.courses.interfaces import ICourseSectionExporter
 
 from nti.contenttypes.courses.exporter import BaseSectionExporter
 
-from nti.contenttypes.courses.utils import get_course_subinstances
+from nti.contenttypes.courses.utils import get_course_hierarchy
 
 from nti.externalization.externalization import to_external_object
 
 from nti.externalization.interfaces import LocatedExternalDict
 from nti.externalization.interfaces import StandardExternalFields
 
+from nti.ntiids.ntiids import get_parts
+from nti.ntiids.ntiids import make_ntiid
+from nti.ntiids.ntiids import make_specific_safe
+
 ITEMS = StandardExternalFields.ITEMS
+NTIID = StandardExternalFields.NTIID
 
 @interface.implementer(ICourseSectionExporter)
 class EvaluationsExporter(BaseSectionExporter):
 
-	def _output(self, course, target_filer=None):
+	def hexdigest(self, data):
+		hasher = hashlib.sha256()
+		hasher.update(data)
+		return hasher.hexdigest()
+
+	def _change_ntiid(self, ext_obj):
+		if isinstance(ext_obj, Mapping):
+			# when not backing up make sure we take a hash of the current NTIID and
+			# use it as the specific part for a new NTIID to make sure there are
+			# fewer collisions when importing back
+			for name in (NTIID, NTIID.lower()):
+				if name in ext_obj:
+					ntiid = ext_obj[name]
+					parts = get_parts(ntiid)
+					digest = self.hexdigest(ntiid)
+					specific = make_specific_safe("%s_%04d" % (digest, len(ntiid)))
+					ntiid = make_ntiid(parts.date, 
+									   parts.provider,
+									   parts.nttype, 
+									   specific=specific)
+					ext_obj[name] = ntiid
+			for value in ext_obj.values():
+				self._change_ntiid(value)
+		elif isinstance(ext_obj, (list, tuple, set)):
+			for value in ext_obj:
+				self._change_ntiid(value)
+
+	def _output(self, course, target_filer=None, backup=True):
 		evaluations = ICourseEvaluations(course)
 		source_filer = get_course_filer(course)
 
@@ -58,25 +93,27 @@ class EvaluationsExporter(BaseSectionExporter):
 				evaluation = copy_evaluation(evaluation)
 				export_evaluation_content(evaluation, source_filer, target_filer)
 			ext_obj = to_external_object(evaluation, name="exporter", decorate=False)
+			if not backup:
+				self._change_ntiid(ext_obj)
 			return ext_obj
 
 		ordered = sorted(evaluations.values(), key=_get_key)
 		return map(_ext, ordered)
 
-	def externalize(self, context, filer=None):
+	def externalize(self, context, filer=None, backup=True):
 		result = LocatedExternalDict()
 		course = ICourseInstance(context)
-		items = self._output(course, target_filer=filer)
+		items = self._output(course, target_filer=filer, backup=backup)
 		if items: # check
 			result[ITEMS] = items
 		return result
 
 	def export(self, context, filer, backup=True):
 		course = ICourseInstance(context)
-		courses = (course,) + tuple(get_course_subinstances(course))
+		courses = get_course_hierarchy(course)
 		for course in courses:
 			bucket = self.course_bucket(course)
-			result = self.externalize(course, filer)
+			result = self.externalize(course, filer, backup)
 			if result:  # check
 				source = self.dump(result)
 				filer.save("evaluation_index.json", source, bucket=bucket,

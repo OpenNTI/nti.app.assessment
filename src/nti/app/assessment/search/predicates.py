85b7059c9e173002d21f7513d50f84a764860434
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-16 00:48:11 +0000

    fix predicate
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@100822 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/search/predicates.py
+++ src/nti/app/assessment/search/predicates.py
@@ -12,31 +12,32 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope import interface
 
-from nti.app.assessment.interfaces import IUsersCourseAssignmentHistoryItem
 from nti.app.assessment.interfaces import IUsersCourseAssignmentHistoryItemFeedback
 
 from nti.contentsearch.interfaces import ISearchHitPredicate
+from nti.contentsearch.predicates import DefaultSearchHitPredicate
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
-from nti.contenttypes.courses.interfaces import ICourseEnrollments
 
-from nti.dataserver.interfaces import IUser
+from nti.contenttypes.courses.utils import is_instructed_by_name
+
+from nti.dataserver.users import User
 
 from nti.traversal.traversal import find_interface
 
 @interface.implementer(ISearchHitPredicate)
 @component.adapter(IUsersCourseAssignmentHistoryItemFeedback)
-class _AssignmentFeedbackItemSearchHitPredicate(object):
-
-	def __init__(self, *args):
-		pass
+class _AssignmentFeedbackItemSearchHitPredicate(DefaultSearchHitPredicate):
 
 	def allow(self, feedback, score, query=None):
-		result = True  # by default allow
-		course = find_interface(feedback, ICourseInstance, strict=False)
-		item = find_interface(feedback, IUsersCourseAssignmentHistoryItem, strict=False)
-		user = IUser(item, None)  # get the user enrolled
-		if course is not None and user is not None:
-			enrollments = ICourseEnrollments(course)
-			result = enrollments.get_enrollment_for_principal(user) is not None
-		return result
+		if self.principal is None:
+			return True
+		else:
+			pid = self.principal.id
+			user = User.get_user(pid)
+			owner = feedback.creator
+			course = find_interface(feedback, ICourseInstance, strict=False)
+			if 		user is not None \
+				and (owner == user is not None or is_instructed_by_name(course, pid)):
+				return True
+		return False

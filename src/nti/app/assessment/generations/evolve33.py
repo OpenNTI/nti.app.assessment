66513ef2b8a2d18de2930a1b2376d04eefb5b8c6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-20 21:45:23 +0000

    add evolve 33 to metaindex missing submissions
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@109198 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/assessment/generations/evolve33.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 33
+
+from zope import component
+from zope import interface
+
+from zope.intid.interfaces import IIntIds
+
+from zope.component.hooks import site
+from zope.component.hooks import setHooks
+from zope.component.hooks import site as current_site
+
+from nti.app.assessment.index import IX_CREATOR
+from nti.app.assessment.index import install_submission_catalog
+
+from nti.app.assessment.interfaces import IUsersCourseInquiries
+from nti.app.assessment.interfaces import IUsersCourseAssignmentHistories
+from nti.app.assessment.interfaces import IUsersCourseAssignmentMetadataContainer
+
+from nti.contenttypes.courses.interfaces import ICourseCatalog
+from nti.contenttypes.courses.interfaces import ICourseInstance
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
+
+from nti.metadata import metadata_queue
+
+from nti.site.hostpolicy import get_all_host_sites
+
+
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+    root = None
+
+    def get_by_oid(self, oid, ignore_creator=False):
+        resolver = component.queryUtility(IOIDResolver)
+        if resolver is None:
+            logger.warn("Using dataserver without a proper ISiteManager.")
+        else:
+            return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+        return None
+
+CONTAINER_INTERFACES = (IUsersCourseInquiries,
+                        IUsersCourseAssignmentHistories,
+                        IUsersCourseAssignmentMetadataContainer)
+
+
+def add_2_queue(context, queue, intids):
+    doc_id = intids.queryId(context)
+    if doc_id is not None:
+        try:
+            queue.add(doc_id)
+        except TypeError:
+            pass
+
+
+def process_course(course, queue, intids):
+    for provided in CONTAINER_INTERFACES:
+        user_data = provided(course, None)
+        if not user_data:
+            continue
+        for item in user_data.values():
+            add_2_queue(item, queue, intids)
+
+
+def process_site_courses(seen, queue, intids):
+    catalog = component.queryUtility(ICourseCatalog)
+    if catalog is None or catalog.isEmpty():
+        return
+    for entry in catalog.iterCatalogEntries():
+        course = ICourseInstance(entry, None)
+        doc_id = intids.queryId(course)
+        if doc_id is None or doc_id in seen:
+            continue
+        seen.add(doc_id)
+        process_course(course, queue, intids)
+
+
+def do_evolve(context, generation=generation):
+    logger.info("Assessment evolution %s started", generation)
+
+    setHooks()
+    conn = context.connection
+    ds_folder = conn.root()['nti.dataserver']
+    lsm = ds_folder.getSiteManager()
+
+    mock_ds = MockDataserver()
+    mock_ds.root = ds_folder
+    component.provideUtility(mock_ds, IDataserver)
+
+    with current_site(ds_folder):
+        assert  component.getSiteManager() == ds_folder.getSiteManager(), \
+                "Hooks not installed?"
+
+        intids = lsm.getUtility(IIntIds)
+        queue = metadata_queue()
+        submission_catalog = install_submission_catalog(ds_folder, intids)
+        submission_catalog[IX_CREATOR].clear()  # clear all
+
+        seen = set()
+        for site in get_all_host_sites():
+            with current_site(site):
+                process_site_courses(seen, queue, intids)
+        seen.clear()
+
+    component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
+    logger.info('Assessment evolution %s done.', generation)
+
+
+def evolve(context):
+    """
+    Evolve to generation 33 by indexing missing submissions
+    """
+    do_evolve(context)
--- src/nti/app/assessment/generations/install.py
+++ src/nti/app/assessment/generations/install.py
@@ -11,7 +11,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 32
+generation = 33
 
 from zope.generations.generations import SchemaManager
 
@@ -20,24 +20,28 @@ from zope.intid.interfaces import IIntIds
 from nti.app.assessment.index import install_evaluation_catalog
 from nti.app.assessment.index import install_submission_catalog
 
+
 class _AssessmentSchemaManager(SchemaManager):
-	"""
-	A schema manager that we can register as a utility in ZCML.
-	"""
-	def __init__(self):
-		super(_AssessmentSchemaManager, self).__init__(
-					generation=generation,
-					minimum_generation=generation,
-					package_name='nti.app.assessment.generations')
+    """
+    A schema manager that we can register as a utility in ZCML.
+    """
+
+    def __init__(self):
+        super(_AssessmentSchemaManager, self).__init__(
+            generation=generation,
+            minimum_generation=generation,
+            package_name='nti.app.assessment.generations')
+
 
 def evolve(context):
-	install_catalog(context)
+    install_catalog(context)
+
 
 def install_catalog(context):
-	conn = context.connection
-	root = conn.root()
-	dataserver_folder = root['nti.dataserver']
-	lsm = dataserver_folder.getSiteManager()
-	intids = lsm.getUtility(IIntIds)
-	install_submission_catalog(dataserver_folder, intids)
-	install_evaluation_catalog(dataserver_folder, intids)
+    conn = context.connection
+    root = conn.root()
+    dataserver_folder = root['nti.dataserver']
+    lsm = dataserver_folder.getSiteManager()
+    intids = lsm.getUtility(IIntIds)
+    install_submission_catalog(dataserver_folder, intids)
+    install_evaluation_catalog(dataserver_folder, intids)

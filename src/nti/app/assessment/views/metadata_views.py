0c9c254b7e2bdc828e690b9fa7e2aa841f9b7d6d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-21 23:14:44 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@109292 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/views/metadata_views.py
+++ src/nti/app/assessment/views/metadata_views.py
@@ -56,192 +56,203 @@ from nti.externalization.oids import to_external_ntiid_oid
 
 from nti.property.property import Lazy
 
+
 @view_config(route_name="objects.generic.traversal",
-			 context=IQAssignment,
-			 renderer='rest',
-			 request_method='POST',
-			 permission=nauth.ACT_READ,
-			 name="Metadata")
+             context=IQAssignment,
+             renderer='rest',
+             request_method='POST',
+             permission=nauth.ACT_READ,
+             name="Metadata")
 class AssignmentSubmissionMetataPostView(AbstractAuthenticatedView,
-								   		 ModeledContentUploadRequestUtilsMixin):
+                                         ModeledContentUploadRequestUtilsMixin):
 
-	_EXTRA_INPUT_ERRORS = \
-			ModeledContentUploadRequestUtilsMixin._EXTRA_INPUT_ERRORS + (AttributeError,)
+    _EXTRA_INPUT_ERRORS = ModeledContentUploadRequestUtilsMixin._EXTRA_INPUT_ERRORS + \
+                          (AttributeError,)
 
-	content_predicate = IUsersCourseAssignmentMetadataItem.providedBy
+    content_predicate = IUsersCourseAssignmentMetadataItem.providedBy
 
-	def _validate(self):
-		creator = self.remoteUser
-		if not creator:
-			raise hexc.HTTPForbidden(_("Must be Authenticated."))
-		course = get_course_from_request(self.request)
-		if course is None:
-			course = get_course_from_evaluation(self.context, creator, exc=False)
-		if course is None:
-			raise hexc.HTTPForbidden(_("Must be enrolled in a course."))
-		return creator, course
+    def _validate(self):
+        creator = self.remoteUser
+        if not creator:
+            raise hexc.HTTPForbidden(_("Must be Authenticated."))
+        course = get_course_from_request(self.request)
+        if course is None:
+            course = get_course_from_evaluation(
+                self.context, creator, exc=False)
+        if course is None:
+            raise hexc.HTTPForbidden(_("Must be enrolled in a course."))
+        return creator, course
 
-	def _process(self, creator=None, course=None, item=None):
-		if creator is None or course is None:
-			creator, course = self._validate()
+    def _process(self, creator=None, course=None, item=None):
+        if creator is None or course is None:
+            creator, course = self._validate()
 
-		item = self.readCreateUpdateContentObject(creator) if item is None else item
-		lifecycleevent.created(item)
+        item = self.readCreateUpdateContentObject(
+            creator) if item is None else item
+        lifecycleevent.created(item)
 
-		self.request.response.status_int = 201
+        self.request.response.status_int = 201
 
-		assignmentId = self.context.ntiid
-		metadata = component.getMultiAdapter((course, creator),
-											 IUsersCourseAssignmentMetadata)
-		item.containerId = assignmentId
-		result = recorded = metadata.append(assignmentId, item)
+        assignmentId = self.context.ntiid
+        metadata = component.getMultiAdapter((course, creator),
+                                             IUsersCourseAssignmentMetadata)
+        item.containerId = assignmentId
+        result = recorded = metadata.append(assignmentId, item)
 
-		result = to_external_object(result)
-		result['href'] = "/%s/Objects/%s" % (get_ds2(self.request),
-											 to_external_ntiid_oid(recorded))
-		interface.alsoProvides(result, INoHrefInResponse)
+        result = to_external_object(result)
+        result['href'] = "/%s/Objects/%s" % (get_ds2(self.request),
+                                             to_external_ntiid_oid(recorded))
+        interface.alsoProvides(result, INoHrefInResponse)
 
-		return result
+        return result
 
-	def _do_call(self):
-		return self._process()
+    def _do_call(self):
+        return self._process()
+
 
 @view_config(route_name="objects.generic.traversal",
-			 context=IQAssignment,
-			 renderer='rest',
-			 request_method='POST',
-			 permission=nauth.ACT_READ,
-			 name="Commence")
+             context=IQAssignment,
+             renderer='rest',
+             request_method='POST',
+             permission=nauth.ACT_READ,
+             name="Commence")
 class AssignmentSubmissionStartPostView(AssignmentSubmissionMetataPostView):
 
-	def _do_call(self):
-		creator, course = self._validate()
-		container = component.getMultiAdapter((course, creator),
-											  IUsersCourseAssignmentMetadata)
-		try:
-			item = container[self.context.ntiid]
-		except KeyError:
-			item = UsersCourseAssignmentMetadataItem()
-			self._process(creator=creator, course=course, item=item)
-		if not item.StartTime:
-			item.StartTime = time.time()
-		# return assignment
-		return self.context
+    def _do_call(self):
+        creator, course = self._validate()
+        container = component.getMultiAdapter((course, creator),
+                                              IUsersCourseAssignmentMetadata)
+        try:
+            item = container[self.context.ntiid]
+        except KeyError:
+            item = UsersCourseAssignmentMetadataItem()
+            self._process(creator=creator, course=course, item=item)
+        if not item.StartTime:
+            item.StartTime = time.time()
+        # return assignment
+        return self.context
+
 
 @view_config(route_name="objects.generic.traversal",
-			 context=IQAssignment,
-			 renderer='rest',
-			 request_method='GET',
-			 permission=nauth.ACT_READ,
-			 name="Metadata")
+             context=IQAssignment,
+             renderer='rest',
+             request_method='GET',
+             permission=nauth.ACT_READ,
+             name="Metadata")
 class AssignmentSubmissionMetadataGetView(AbstractAuthenticatedView):
 
-	@Lazy
-	def course(self):
-		course = get_course_from_request(self.request)
-		if course is None:
-			course = get_course_from_evaluation(self.context, self.remoteUser, exc=False)
-		return course
+    @Lazy
+    def course(self):
+        course = get_course_from_request(self.request)
+        if course is None:
+            course = get_course_from_evaluation(
+                self.context, self.remoteUser, exc=False)
+        return course
 
-	def _do_call(self):
-		creator = self.remoteUser
-		if not creator:
-			raise hexc.HTTPForbidden(_("Must be Authenticated."))
+    def _do_call(self):
+        creator = self.remoteUser
+        if not creator:
+            raise hexc.HTTPForbidden(_("Must be Authenticated."))
 
-		if self.course is None:
-			raise hexc.HTTPForbidden(_("Must be enrolled in a course."))
+        if self.course is None:
+            raise hexc.HTTPForbidden(_("Must be enrolled in a course."))
 
-		container = component.getMultiAdapter((self.course, creator),
-											  IUsersCourseAssignmentMetadata)
+        container = component.getMultiAdapter((self.course, creator),
+                                              IUsersCourseAssignmentMetadata)
 
-		result = container[self.context.ntiid]
-		return result
+        result = container[self.context.ntiid]
+        return result
 
-	def __call__(self):
-		try:
-			result = self._do_call()
-			return result
-		except KeyError:
-			return hexc.HTTPNotFound()
+    def __call__(self):
+        try:
+            result = self._do_call()
+            return result
+        except KeyError:
+            return hexc.HTTPNotFound()
+
 
 @view_config(route_name="objects.generic.traversal",
-			 context=IQAssignment,
-			 renderer='rest',
-			 request_method='GET',
-			 permission=nauth.ACT_READ,
-			 name="StartTime")
+             context=IQAssignment,
+             renderer='rest',
+             request_method='GET',
+             permission=nauth.ACT_READ,
+             name="StartTime")
 class AssignmentSubmissionStartGetView(AssignmentSubmissionMetadataGetView):
 
-	def __call__(self):
-		try:
-			item = self._do_call()
-			result = LocatedExternalDict({'StartTime': item.StartTime})
-			return result
-		except KeyError:
-			return hexc.HTTPNotFound()
+    def __call__(self):
+        try:
+            item = self._do_call()
+            result = LocatedExternalDict({'StartTime': item.StartTime})
+            return result
+        except KeyError:
+            return hexc.HTTPNotFound()
+
 
 @view_config(route_name="objects.generic.traversal",
-			 context=IQTimedAssignment,
-			 renderer='rest',
-			 request_method='GET',
-			 permission=nauth.ACT_READ,
-			 name="TimeRemaining")
+             context=IQTimedAssignment,
+             renderer='rest',
+             request_method='GET',
+             permission=nauth.ACT_READ,
+             name="TimeRemaining")
 class AssignmentTimeRemainingGetView(AssignmentSubmissionMetadataGetView):
-	"""
-	Return the time remaining in seconds until this `IQTimedAssignment` is
-	due. We may return negative numbers to indicate past due status.
-	"""
+    """
+    Return the time remaining in seconds until this `IQTimedAssignment` is
+    due. We may return negative numbers to indicate past due status.
+    """
 
-	def _get_time_remaining(self, metadata):
-		# We return None if the assignment is not yet started.
-		result = None
-		if metadata.StartTime:
-			max_time_allowed = get_max_time_allowed( self.context, self.course )
-			if max_time_allowed:
-				now = time.time()
-				# Both in seconds
-				end_point = metadata.StartTime + max_time_allowed
-				result = end_point - now
-		return result
+    def _get_time_remaining(self, metadata):
+        # We return None if the assignment is not yet started.
+        result = None
+        if metadata.StartTime:
+            max_time_allowed = get_max_time_allowed(self.context, self.course)
+            if max_time_allowed:
+                now = time.time()
+                # Both in seconds
+                end_point = metadata.StartTime + max_time_allowed
+                result = end_point - now
+        return result
 
-	def __call__(self):
-		try:
-			item = self._do_call()
-			time_remaining = self._get_time_remaining( item )
-			result = LocatedExternalDict({'TimeRemaining': time_remaining})
-			return result
-		except KeyError:
-			return hexc.HTTPNotFound()
+    def __call__(self):
+        try:
+            item = self._do_call()
+            time_remaining = self._get_time_remaining(item)
+            result = LocatedExternalDict({'TimeRemaining': time_remaining})
+            return result
+        except KeyError:
+            return hexc.HTTPNotFound()
+
 
 @view_config(route_name="objects.generic.traversal",
-			 renderer='rest',
-			 context=IUsersCourseAssignmentMetadataContainer,
-			 permission=nauth.ACT_READ,
-			 request_method='GET')
+             renderer='rest',
+             context=IUsersCourseAssignmentMetadataContainer,
+             permission=nauth.ACT_READ,
+             request_method='GET')
 class AssignmentMetadataGetView(AbstractAuthenticatedView):
-	"""
-	Students can view their assignment metadata  as ``path/to/course/AssignmentMetadata``
-	"""
+    """
+    Students can view their assignment metadata  as ``path/to/course/AssignmentMetadata``
+    """
 
-	def __call__(self):
-		container = self.request.context
-		return container
+    def __call__(self):
+        container = self.request.context
+        return container
+
 
 @view_config(route_name="objects.generic.traversal",
-			 context=IUsersCourseAssignmentMetadataItem,
-			 renderer='rest',
-			 permission=nauth.ACT_UPDATE,
-			 request_method='PUT')
+             context=IUsersCourseAssignmentMetadataItem,
+             renderer='rest',
+             permission=nauth.ACT_UPDATE,
+             request_method='PUT')
 class AssignmentMetadataItemPutView(UGDPutView):
-	pass
+    pass
+
 
 @view_config(route_name="objects.generic.traversal",
-			 context=IUsersCourseAssignmentMetadataItem,
-			 renderer='rest',
-			 permission=nauth.ACT_DELETE,
-			 request_method='DELETE')
+             context=IUsersCourseAssignmentMetadataItem,
+             renderer='rest',
+             permission=nauth.ACT_DELETE,
+             request_method='DELETE')
 class AssignmentMetadataItemDeleteView(UGDDeleteView):
 
-	def _do_delete_object(self, theObject):
-		del theObject.__parent__[theObject.__name__]
-		return theObject
+    def _do_delete_object(self, theObject):
+        del theObject.__parent__[theObject.__name__]
+        return theObject
--- src/nti/app/assessment/views/savepoint_views.py
+++ src/nti/app/assessment/views/savepoint_views.py
@@ -59,136 +59,146 @@ from nti.externalization.externalization import to_external_object
 
 from nti.externalization.oids import to_external_ntiid_oid
 
+
 @view_config(route_name="objects.generic.traversal",
-			 context=IQAssignment,
-			 renderer='rest',
-			 request_method='POST',
-			 permission=nauth.ACT_READ,
-			 name="Savepoint")
+             context=IQAssignment,
+             renderer='rest',
+             request_method='POST',
+             permission=nauth.ACT_READ,
+             name="Savepoint")
 class AssignmentSubmissionSavepointPostView(AbstractAuthenticatedView,
-								   			ModeledContentUploadRequestUtilsMixin):
-	"""
-	Students can POST to the assignment to create their save point
-	"""
+                                            ModeledContentUploadRequestUtilsMixin):
+    """
+    Students can POST to the assignment to create their save point
+    """
 
-	_EXTRA_INPUT_ERRORS = ModeledContentUploadRequestUtilsMixin._EXTRA_INPUT_ERRORS + \
-						  (AttributeError,)
+    _EXTRA_INPUT_ERRORS = ModeledContentUploadRequestUtilsMixin._EXTRA_INPUT_ERRORS + \
+                          (AttributeError,)
 
-	content_predicate = IQAssignmentSubmission.providedBy
+    content_predicate = IQAssignmentSubmission.providedBy
 
-	def _do_call(self):
-		creator = self.remoteUser
-		if not creator:
-			raise hexc.HTTPForbidden(_("Must be Authenticated."))
+    def _do_call(self):
+        creator = self.remoteUser
+        if not creator:
+            raise hexc.HTTPForbidden(_("Must be Authenticated."))
 
-		course = get_course_from_request(self.request)
-		if course is None:
-			course = get_course_from_evaluation(self.context, creator, exc=False)
-		if course is None:
-			raise hexc.HTTPForbidden(_("Must be enrolled in a course."))
+        course = get_course_from_request(self.request)
+        if course is None:
+            course = get_course_from_evaluation(
+                self.context, creator, exc=False)
+        if course is None:
+            raise hexc.HTTPForbidden(_("Must be enrolled in a course."))
 
-		if not is_assignment_available( self.context, user=creator, course=course ):
-			raise hexc.HTTPForbidden(_("Assignment is not available."))
+        if not is_assignment_available(self.context, user=creator, course=course):
+            raise hexc.HTTPForbidden(_("Assignment is not available."))
 
-		if not self.request.POST:
-			submission = self.readCreateUpdateContentObject(creator)
-			check_upload_files(submission)
-		else:
-			# try legacy submssion.
-			extValue = get_source(self.request, 'json')
-			if extValue:
-				extValue = extValue.read()
-				extValue = read_input_data(extValue, self.request)
-			else:
-				extValue = read_body_as_external_object(self.request)
-			if not extValue:
-				raise hexc.HTTPUnprocessableEntity(_("No submission source was specified."))
-			submission = self.readCreateUpdateContentObject(creator,
-															externalValue=extValue)
-			submission = read_multipart_sources(submission, self.request)
+        if not self.request.POST:
+            submission = self.readCreateUpdateContentObject(creator)
+            check_upload_files(submission)
+        else:
+            # try legacy submssion.
+            extValue = get_source(self.request, 'json')
+            if extValue:
+                extValue = extValue.read()
+                extValue = read_input_data(extValue, self.request)
+            else:
+                extValue = read_body_as_external_object(self.request)
+            if not extValue:
+                msg =  _("No submission source was specified.")
+                raise hexc.HTTPUnprocessableEntity(msg)
+            submission = self.readCreateUpdateContentObject(creator,
+                                                            externalValue=extValue)
+            submission = read_multipart_sources(submission, self.request)
 
-		# Must check version before checking timed commence status.
-		check_submission_version(submission, self.context)
+        # Must check version before checking timed commence status.
+        check_submission_version(submission, self.context)
 
-		# No savepoints unless the timed assignment has been started
-		if IQTimedAssignment.providedBy(self.context):
-			item = get_assessment_metadata_item(course,
-												self.remoteUser,
-												self.context.ntiid)
-			if item is None or not item.StartTime:
-				raise hexc.HTTPClientError(_("Cannot savepoint timed assignment unless started."))
+        # No savepoints unless the timed assignment has been started
+        if IQTimedAssignment.providedBy(self.context):
+            item = get_assessment_metadata_item(course,
+                                                self.remoteUser,
+                                                self.context.ntiid)
+            if item is None or not item.StartTime:
+                msg = _("Cannot savepoint timed assignment unless started.")
+                raise hexc.HTTPClientError(msg)
 
-		savepoint = component.getMultiAdapter((course, submission.creator),
-											   IUsersCourseAssignmentSavepoint)
-		submission.containerId = submission.assignmentId
+        savepoint = component.getMultiAdapter((course, submission.creator),
+                                              IUsersCourseAssignmentSavepoint)
+        submission.containerId = submission.assignmentId
 
-		# for legacy purposes we assume the start time as the first savepoint submitted
-		metadata = component.getMultiAdapter((course, submission.creator),
-											  IUsersCourseAssignmentMetadata)
-		metadata.get_or_create(submission.assignmentId, time.time())
+        # for legacy purposes we assume the start time as the first savepoint
+        # submitted
+        metadata = component.getMultiAdapter((course, submission.creator),
+                                             IUsersCourseAssignmentMetadata)
+        metadata.get_or_create(submission.assignmentId, time.time())
 
-		version = self.context.version
-		if version is not None: # record version
-			submission.version = version
+        version = self.context.version
+        if version is not None:  # record version
+            submission.version = version
 
-		# Now record the submission.
-		self.request.response.status_int = 201
-		result = recorded = savepoint.recordSubmission(submission)
-		result = to_external_object(result)
-		result['href'] = "/%s/Objects/%s" % (get_ds2(self.request),
-											 to_external_ntiid_oid(recorded))
-		interface.alsoProvides(result, INoHrefInResponse)
-		return result
+        # Now record the submission.
+        self.request.response.status_int = 201
+        result = recorded = savepoint.recordSubmission(submission)
+        result = to_external_object(result)
+        result['href'] = "/%s/Objects/%s" % (get_ds2(self.request),
+                                             to_external_ntiid_oid(recorded))
+        interface.alsoProvides(result, INoHrefInResponse)
+        return result
+
 
 @view_config(route_name="objects.generic.traversal",
-			 context=IQAssignment,
-			 renderer='rest',
-			 request_method='GET',
-			 permission=nauth.ACT_READ,
-			 name="Savepoint")
+             context=IQAssignment,
+             renderer='rest',
+             request_method='GET',
+             permission=nauth.ACT_READ,
+             name="Savepoint")
 class AssignmentSubmissionSavepointGetView(AbstractAuthenticatedView):
 
-	def __call__(self):
-		creator = self.remoteUser
-		if not creator:
-			raise hexc.HTTPForbidden(_("Must be Authenticated."))
+    def __call__(self):
+        creator = self.remoteUser
+        if not creator:
+            raise hexc.HTTPForbidden(_("Must be Authenticated."))
 
-		course = get_course_from_request(self.request)
-		if course is None:
-			course = get_course_from_evaluation(self.context, creator, exc=False)
-		if course is None:
-			raise hexc.HTTPForbidden(_("Must be enrolled in a course."))
+        course = get_course_from_request(self.request)
+        if course is None:
+            course = get_course_from_evaluation(self.context, 
+                                                 creator, 
+                                                 exc=False)
+        if course is None:
+            raise hexc.HTTPForbidden(_("Must be enrolled in a course."))
 
-		__traceback_info__ = creator, course
-		savepoint = component.getMultiAdapter((course, creator),
-											  IUsersCourseAssignmentSavepoint)
-		try:
-			result = savepoint[self.context.ntiid]
-			return result
-		except KeyError:
-			return hexc.HTTPNotFound()
+        __traceback_info__ = creator, course
+        savepoint = component.getMultiAdapter((course, creator),
+                                              IUsersCourseAssignmentSavepoint)
+        try:
+            result = savepoint[self.context.ntiid]
+            return result
+        except KeyError:
+            return hexc.HTTPNotFound()
+
 
 @view_config(route_name="objects.generic.traversal",
-			 renderer='rest',
-			 context=IUsersCourseAssignmentSavepoints,
-			 permission=nauth.ACT_READ,
-			 request_method='GET')
+             renderer='rest',
+             context=IUsersCourseAssignmentSavepoints,
+             permission=nauth.ACT_READ,
+             request_method='GET')
 class AssignmentSavepointsGetView(AbstractAuthenticatedView):
-	"""
-	Students can view their assignment save points as ``path/to/course/AssignmentSavepoints``
-	"""
+    """
+    Students can view their assignment save points as ``path/to/course/AssignmentSavepoints``
+    """
 
-	def __call__(self):
-		savepoints = self.request.context
-		return savepoints
+    def __call__(self):
+        savepoints = self.request.context
+        return savepoints
+
 
 @view_config(route_name="objects.generic.traversal",
-			 context=IUsersCourseAssignmentSavepointItem,
-			 renderer='rest',
-			 permission=nauth.ACT_DELETE,
-			 request_method='DELETE')
+             context=IUsersCourseAssignmentSavepointItem,
+             renderer='rest',
+             permission=nauth.ACT_DELETE,
+             request_method='DELETE')
 class AssignmentSavepointItemDeleteView(UGDDeleteView):
 
-	def _do_delete_object(self, theObject):
-		del theObject.__parent__[theObject.__name__]
-		return theObject
+    def _do_delete_object(self, theObject):
+        del theObject.__parent__[theObject.__name__]
+        return theObject

20aeff9e26d89157e9b351f8162c4faa724635ee
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-13 14:45:33 +0000

    restore code and make sure code is reachable
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@112686 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/importer.py
+++ src/nti/app/assessment/importer.py
@@ -44,25 +44,25 @@ class AssessmentsImporter(BaseSectionImporter):
     def remove_assessments(self, package):
         remove_assessment_items_from_oldcontent(package, force=True)
 
-    def process_package(self, source, package, filer, writeout):
-        site = get_host_site(get_resource_site_name(package))
-        with current_site(site):
-            self.remove_assessments(package)
-            return populate_question_map_json(source, package)
-        # save source
-        if writeout and IFilesystemBucket.providedBy(package.root):
-            source = filer.get(self.ASSESSMENT_INDEX)  # reload
-            self.makedirs(package.root.absolute_path)  # create
-            new_path = os.path.join(package.root.absolute_path,
-                                    self.ASSESSMENT_INDEX)
-            transfer_to_native_file(source, new_path)
-
-    def process(self, context, filer, writeout=True):
+    def process(self, context, filer, writeout=False):
         course = ICourseInstance(context)
         course = get_parent_course(course)
         source = filer.get(self.ASSESSMENT_INDEX)
         if source is not None:
+            result = set()
             source = self.load(source)
             for package in get_course_packages(course):
-                self.process_package(source, package, filer, writeout)
+                site = get_resource_site_name(package)
+                site = get_host_site(site)
+                with current_site(site):
+                    self.remove_assessments(package)
+                    result.update(populate_question_map_json(source, package))
+                # save source
+                if writeout and IFilesystemBucket.providedBy(package.root):
+                    source = filer.get(self.ASSESSMENT_INDEX)  # reload
+                    self.makedirs(package.root.absolute_path)  # create
+                    new_path = os.path.join(package.root.absolute_path,
+                                            self.ASSESSMENT_INDEX)
+                    transfer_to_native_file(source, new_path)
+            return result
         return ()

181331f26865364f17e53353ce567961dad3fbee
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-12 02:50:54 +0000

    typo
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@117126 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/tests/test_survey.py
+++ src/nti/app/assessment/tests/test_survey.py
@@ -295,7 +295,7 @@ class TestSurveyViews(RegisterAssignmentLayerMixin, ApplicationLayerTest):
     def test_submission_metadata(self, fake_active, fake_date):
         fake_active.is_callable().returns(True)
         fake_date.is_callable().returns('05-10-2017')
-        test_student_environ = self._make_extra_environ(sername='test_student')
+        test_student_environ = self._make_extra_environ(username='test_student')
 
         test_student_environ.update({'HTTP_ORIGIN': 'http://janux.ou.edu'})
         instructor_environ = self._make_extra_environ(username='harp4162')

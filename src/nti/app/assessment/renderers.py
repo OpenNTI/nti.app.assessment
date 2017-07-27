7504b5d0480dd0ac87e1c23df6680cc858be1937
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-27 21:03:23 +0000

    check for pkg in request
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@118285 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/renderers.py
+++ src/nti/app/assessment/renderers.py
@@ -15,6 +15,7 @@ from zope import interface
 from zope.location.interfaces import ILocation
 
 from nti.app.assessment.utils import get_course_from_request
+from nti.app.assessment.utils import get_package_from_request
 
 from nti.appserver.interfaces import IEditLinkMaker
 
@@ -32,16 +33,17 @@ class AssesmentEditLinkMaker(DefaultEditLinkMaker):
     def make(self, context, request=None,
              allow_traversable_paths=True, link_method=None):
         context = self.context if context is None else context
-        course = get_course_from_request(request)
-        if course is not None:
-            link = Link(course,
-                        rel='edit',
-                        elements=('Assessments', context.ntiid),
-                        method=link_method)
-            link.__parent__ = context.__parent__
-            link.__name__ = context.__name__
-            interface.alsoProvides(link, ILocation)
-            return link
+        for composite in (get_course_from_request(request), 
+                          get_package_from_request(request)):
+            if composite is not None:
+                link = Link(composite,
+                            rel='edit',
+                            elements=('Assessments', context.ntiid),
+                            method=link_method)
+                link.__parent__ = context.__parent__
+                link.__name__ = context.__name__
+                interface.alsoProvides(link, ILocation)
+                return link
         # default
         return DefaultEditLinkMaker.make(self,
                                          context,

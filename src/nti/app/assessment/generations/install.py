775ae46f03e900212564cbbe3d9d59e2558e88bd
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-28 03:20:56 +0000

    enable evol 39
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@118306 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/generations/install.py
+++ src/nti/app/assessment/generations/install.py
@@ -11,7 +11,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 38
+generation = 39
 
 from zope.generations.generations import SchemaManager
 

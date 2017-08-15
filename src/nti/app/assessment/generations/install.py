7c630e40b58342f5d1fa9d5cec57711a9a49eb70
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-15 16:24:38 +0000

    remove bad migration
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@119536 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/generations/install.py
+++ src/nti/app/assessment/generations/install.py
@@ -11,7 +11,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 40
+generation = 39
 
 from zope.generations.generations import SchemaManager
 

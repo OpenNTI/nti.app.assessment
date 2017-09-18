f408d4c3c8857b655769319dcd19ee3cae5c8b59
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-09-18 03:14:19 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.assessment/trunk@121796 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/assessment/tests/__init__.py
+++ src/nti/app/assessment/tests/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 # disable: accessing protected members, too many methods
@@ -22,37 +22,40 @@ from nti.dataserver.tests.mock_dataserver import DSInjectorMixin
 
 import zope.testing.cleanup
 
+
 class SharedConfiguringTestLayer(ZopeComponentLayer,
-								 GCLayerMixin,
-								 ConfiguringLayerMixin,
-								 DSInjectorMixin):
-	set_up_packages = ('nti.dataserver', 'nti.appserver', 'nti.app.assessment')
+                                 GCLayerMixin,
+                                 ConfiguringLayerMixin,
+                                 DSInjectorMixin):
+    set_up_packages = ('nti.dataserver', 'nti.appserver', 'nti.app.assessment')
 
-	@classmethod
-	def setUp(cls):
-		cls.setUpPackages()
-		cls.old_data_dir = os.getenv('DATASERVER_DATA_DIR')
-		cls.new_data_dir = tempfile.mkdtemp(dir="/tmp")
-		os.environ['DATASERVER_DATA_DIR'] = cls.new_data_dir
+    @classmethod
+    def setUp(cls):
+        cls.setUpPackages()
+        cls.old_data_dir = os.getenv('DATASERVER_DATA_DIR')
+        cls.new_data_dir = tempfile.mkdtemp(dir="/tmp")
+        os.environ['DATASERVER_DATA_DIR'] = cls.new_data_dir
 
-	@classmethod
-	def tearDown(cls):
-		cls.tearDownPackages()
-		zope.testing.cleanup.cleanUp()
+    @classmethod
+    def tearDown(cls):
+        cls.tearDownPackages()
+        zope.testing.cleanup.cleanUp()
 
-	@classmethod
-	def testSetUp(cls, test=None):
-		test = test or find_test()
-		cls.setUpTestDS(test)
-		shutil.rmtree(cls.new_data_dir, True)
-		os.environ['DATASERVER_DATA_DIR'] = cls.old_data_dir or '/tmp'
+    @classmethod
+    def testSetUp(cls, test=None):
+        test = test or find_test()
+        cls.setUpTestDS(test)
+        shutil.rmtree(cls.new_data_dir, True)
+        os.environ['DATASERVER_DATA_DIR'] = cls.old_data_dir or '/tmp'
 
-	@classmethod
-	def testTearDown(cls):
-		pass
+    @classmethod
+    def testTearDown(cls):
+        pass
+
 
 class AssessmentLayerTest(unittest.TestCase):
-	layer = SharedConfiguringTestLayer
+    layer = SharedConfiguringTestLayer
+
 
 from zope import component
 
@@ -62,18 +65,14 @@ import ZODB
 
 from nti.app.assessment import get_evaluation_catalog
 
-from nti.app.products.courseware.tests import publish_ou_course_entries
 from nti.app.products.courseware.tests import InstructedCourseApplicationTestLayer
 
 from nti.assessment.assignment import QAssignment
 from nti.assessment.assignment import QAssignmentPart
 
-from nti.assessment.submission import AssignmentSubmission
-from nti.assessment.submission import QuestionSetSubmission
-
-from nti.assessment import interfaces as asm_interfaces
+from nti.assessment.interfaces import IQAssignment
+from nti.assessment.interfaces import IQuestionSet
 from nti.assessment.interfaces import IQAssessmentItemContainer
-from nti.assessment.interfaces import IQAssignmentSubmissionPendingAssessment
 
 from nti.contentlibrary.indexed_data import get_library_catalog
 
@@ -84,149 +83,155 @@ from nti.app.testing.application_webtest import ApplicationTestLayer
 from nti.dataserver.tests.mock_dataserver import WithMockDS
 from nti.dataserver.tests.mock_dataserver import mock_db_trans
 
+
 class RegisterAssignmentLayer(InstructedCourseApplicationTestLayer):
 
-	set_up_packages = (	'nti.dataserver', 'nti.assessment', 'nti.app.assessment',
-						'nti.contenttypes.courses', 'nti.app.products.courseware')
+    set_up_packages = ('nti.dataserver', 'nti.assessment', 'nti.app.assessment',
+                       'nti.contenttypes.courses', 'nti.app.products.courseware')
 
-	@classmethod
-	def _register_assignment(cls):
+    @classmethod
+    def _register_assignment(cls):
 
-		def install_questions():
-			lib = component.getUtility(IContentPackageLibrary)
+        def install_questions():
+            lib = component.getUtility(IContentPackageLibrary)
 
-			poll_ntiid = "tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.pollid.aristotle.1"
-			survey_ntiid = "tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.set.survey:KNOWING_aristotle"
+            poll_ntiid = u"tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.pollid.aristotle.1"
+            survey_ntiid = u"tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.set.survey:KNOWING_aristotle"
 
-			assignment_ntiid = "tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.asg:QUIZ1_aristotle"
-			question_set_id = "tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.set.qset:QUIZ1_aristotle"
+            assignment_ntiid = u"tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.asg:QUIZ1_aristotle"
+            question_set_id = u"tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.set.qset:QUIZ1_aristotle"
 
-			question_set = component.getUtility(asm_interfaces.IQuestionSet,
-												name=question_set_id)
-			question_set.publish( event=False )
+            question_set = component.getUtility(IQuestionSet,
+                                                name=question_set_id)
+            question_set.publish(event=False)
 
-			# add a assignment with a future date
-			due_date = datetime.datetime.today()
-			due_date = due_date + datetime.timedelta(days=365)
+            # add a assignment with a future date
+            due_date = datetime.datetime.today()
+            due_date = due_date + datetime.timedelta(days=365)
 
-			assignment_part = QAssignmentPart(question_set=question_set,
-											  auto_grade=True)
+            assignment_part = QAssignmentPart(question_set=question_set,
+                                              auto_grade=True)
 
-			assignment = QAssignment(parts=(assignment_part,),
-									 available_for_submission_ending=due_date)
-			assignment.__name__ = assignment.ntiid = assignment_ntiid
-			assignment.publish( event=False )
+            assignment = QAssignment(parts=(assignment_part,),
+                                     available_for_submission_ending=due_date)
+            assignment.__name__ = assignment.ntiid = assignment_ntiid
+            assignment.publish(event=False)
 
-			intids = component.getUtility(IIntIds)
-			intids.register(assignment, event=False)
-			component.getSiteManager().registerUtility(assignment,
-													   provided=asm_interfaces.IQAssignment,
-													   name=assignment_ntiid)
+            intids = component.getUtility(IIntIds)
+            intids.register(assignment, event=False)
+            component.getSiteManager().registerUtility(assignment,
+                                                       provided=IQAssignment,
+                                                       name=assignment_ntiid)
 
-			# Make sure our assignment is indexed to our page and course content package.
-			lesson_page_id = "tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.sec:QUIZ_01.01"
-			lesson = lib.pathToNTIID(lesson_page_id)[-1]
-			asm_cont = IQAssessmentItemContainer(lesson)
-			asm_cont.append(assignment)
-			assignment.__parent__ = lesson
-			catalog = get_library_catalog()
+            # Make sure our assignment is indexed to our page and course
+            # content package.
+            lesson_page_id = u"tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.sec:QUIZ_01.01"
+            lesson = lib.pathToNTIID(lesson_page_id)[-1]
+            asm_cont = IQAssessmentItemContainer(lesson)
+            asm_cont.append(assignment)
+            assignment.__parent__ = lesson
+            catalog = get_library_catalog()
 
-			content_package_ntiid = 'tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.clc_3403_law_and_justice'
-			catalog.index(assignment, container_ntiids=(lesson.ntiid, content_package_ntiid))
+            content_package_ntiid = u'tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.clc_3403_law_and_justice'
+            catalog.index(assignment, 
+                          container_ntiids=(lesson.ntiid, content_package_ntiid))
 
-			eval_catalog = get_evaluation_catalog()
-			intid = intids.getId( assignment )
-			eval_catalog.index_doc( intid, assignment )
+            eval_catalog = get_evaluation_catalog()
+            intid = intids.getId(assignment)
+            eval_catalog.index_doc(intid, assignment)
 
-			cls.poll_id = poll_ntiid
-			cls.survey_id = survey_ntiid
+            cls.poll_id = poll_ntiid
+            cls.survey_id = survey_ntiid
 
-			cls.question_set = question_set
-			cls.question_set_id = question_set_id
-			cls.question_id = 'tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.qid.aristotle.1'
+            cls.question_set = question_set
+            cls.question_set_id = question_set_id
+            cls.question_id = u'tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.qid.aristotle.1'
 
-			cls.lesson_page_id = lesson_page_id
-			cls.assignment_id = assignment_ntiid
+            cls.lesson_page_id = lesson_page_id
+            cls.assignment_id = assignment_ntiid
 
-		from nti.contenttypes.courses.interfaces import ICourseCatalog
+        from nti.contenttypes.courses.interfaces import ICourseCatalog
 
-		database = ZODB.DB(ApplicationTestLayer._storage_base, database_name='Users')
+        database = ZODB.DB(ApplicationTestLayer._storage_base,
+                           database_name='Users')
 
-		@WithMockDS(database=database)
-		def _sync():
-			with mock_db_trans(site_name='platform.ou.edu'):
-				install_questions()
-				catalog = component.getUtility(ICourseCatalog)
-				try:
-					from nti.app.products.courseware.interfaces import ICourseInstance
-					from nti.app.products.gradebook.assignments import synchronize_gradebook
-					for c in catalog.iterCatalogEntries():
-						synchronize_gradebook(ICourseInstance(c, None))
-				except ImportError:
-					pass
-		_sync()
+        @WithMockDS(database=database)
+        def _sync():
+            with mock_db_trans(site_name='platform.ou.edu'):
+                install_questions()
+                catalog = component.getUtility(ICourseCatalog)
+                try:
+                    from nti.app.products.courseware.interfaces import ICourseInstance
+                    from nti.app.products.gradebook.assignments import synchronize_gradebook
+                    for c in catalog.iterCatalogEntries():
+                        synchronize_gradebook(ICourseInstance(c, None))
+                except ImportError:
+                    pass
+        _sync()
 
-	@classmethod
-	def setUp(cls):
-		cls._register_assignment()
+    @classmethod
+    def setUp(cls):
+        cls._register_assignment()
 
-	@classmethod
-	def tearDown(cls):
-		# Must implement!
-		pass
+    @classmethod
+    def tearDown(cls):
+        # Must implement!
+        pass
 
-	@classmethod
-	def setUpTest(cls):
-		pass
+    @classmethod
+    def setUpTest(cls):
+        pass
 
-	@classmethod
-	def tearDownTest(cls):
-		pass
+    @classmethod
+    def tearDownTest(cls):
+        pass
+
 
 class RegisterAssignmentsForEveryoneLayer(RegisterAssignmentLayer):
 
-	set_up_packages = ('nti.dataserver', 'nti.appserver', 'nti.app.assessment')
+    set_up_packages = ('nti.dataserver', 'nti.appserver', 'nti.app.assessment')
 
-	@classmethod
-	def setUp(cls):
-		from ..assignment_filters import UserEnrolledForCreditInCourseOrInstructsFilter
-		UserEnrolledForCreditInCourseOrInstructsFilter.TEST_OVERRIDE = True
+    @classmethod
+    def setUp(cls):
+        from nti.app.assessment.assignment_filters import UserEnrolledForCreditInCourseOrInstructsFilter
+        UserEnrolledForCreditInCourseOrInstructsFilter.TEST_OVERRIDE = True
 
-	@classmethod
-	def tearDown(cls):
-		# Must implement!
-		from ..assignment_filters import UserEnrolledForCreditInCourseOrInstructsFilter
-		UserEnrolledForCreditInCourseOrInstructsFilter.TEST_OVERRIDE = False
+    @classmethod
+    def tearDown(cls):
+        # Must implement!
+        from nti.app.assessment.assignment_filters import UserEnrolledForCreditInCourseOrInstructsFilter
+        UserEnrolledForCreditInCourseOrInstructsFilter.TEST_OVERRIDE = False
 
-	@classmethod
-	def setUpTest(cls):
-		pass
+    @classmethod
+    def setUpTest(cls):
+        pass
 
-	@classmethod
-	def tearDownTest(cls):
-		pass
+    @classmethod
+    def tearDownTest(cls):
+        pass
+
 
 class RegisterAssignmentLayerMixin(object):
 
-	poll_id = None
-	survey_id = None
+    poll_id = None
+    survey_id = None
 
-	question_id = None
-	question_set = None
-	question_set_id = None
+    question_id = None
+    question_set = None
+    question_set_id = None
 
-	assignment_id = None
-	lesson_page_id = None
+    assignment_id = None
+    lesson_page_id = None
 
-	def setUp(self):
-		super(RegisterAssignmentLayerMixin, self).setUp()
+    def setUp(self):
+        super(RegisterAssignmentLayerMixin, self).setUp()
 
-		self.poll_id = RegisterAssignmentLayer.poll_id
-		self.survey_id = RegisterAssignmentLayer.survey_id
+        self.poll_id = RegisterAssignmentLayer.poll_id
+        self.survey_id = RegisterAssignmentLayer.survey_id
 
-		self.question_id = RegisterAssignmentLayer.question_id
-		self.question_set = RegisterAssignmentLayer.question_set
-		self.question_set_id = RegisterAssignmentLayer.question_set_id
+        self.question_id = RegisterAssignmentLayer.question_id
+        self.question_set = RegisterAssignmentLayer.question_set
+        self.question_set_id = RegisterAssignmentLayer.question_set_id
 
-		self.lesson_page_id = RegisterAssignmentLayer.lesson_page_id
-		self.assignment_ntiid = self.assignment_id = RegisterAssignmentLayer.assignment_id
+        self.lesson_page_id = RegisterAssignmentLayer.lesson_page_id
+        self.assignment_ntiid = self.assignment_id = RegisterAssignmentLayer.assignment_id

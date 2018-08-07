3b4672e95d2882efeb36ae08a04182a80b963eb7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-06 19:54:59 -0500

    pylint

--- src/nti/app/assessment/completion.py
+++ src/nti/app/assessment/completion.py
@@ -183,6 +183,7 @@ class _AssessmentItemProvider(object):
             uber_filter = get_course_assessment_predicate_for_user(user,
                                                                    self.course)
             # Must grab all assignments in our parent
+            # pylint: disable=redundant-keyword-arg
             assignments = catalog.iter_assignments(course_lineage=True)
             result = [removeAllProxies(x) for x in assignments
                       if uber_filter(x) and self._include_item(x)]

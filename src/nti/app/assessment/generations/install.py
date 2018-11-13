88e94c680b7a19ffad0d9c54724542d6f54bcbd4
Josh Zuech <josh.zuech@nextthought.com>
2018-11-13 16:57:25 -0600

    Alter migration to clean up multiple placeholder submissions

--- src/nti/app/assessment/generations/install.py
+++ src/nti/app/assessment/generations/install.py
@@ -11,7 +11,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 44
+generation = 45
 
 from zope.generations.generations import SchemaManager
 
